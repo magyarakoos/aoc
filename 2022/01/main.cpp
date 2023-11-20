a58ab611b2059a7974dfd26c4a715fff1707da62
magyarakoos <ninjaoffical08@gmail.com>
2023-11-20 19:27:26 +0100

    Update 2022/01

--- 2022/01/main.cpp
+++ 2022/01/main.cpp
@@ -1,28 +1,21 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-    int n;
+    int n = 0;
     vector<int> v;
-    ifstream f("input.txt");
+    ifstream f("input");
     string s;
     while (getline(f, s)) {
-        if (s.empty()) {
+        if (s.size()) {
+            n += stoi(s);
+        } else {
             v.push_back(n);
             n = 0;
         }
-        else {
-            n += stoi(s);
-        }
     }
-    if (n != 0) v.push_back(n);
+    if (n) v.push_back(n);
     sort(v.begin(), v.end(), greater<int>());
-    cout << v[0] << "\n" << v[0]+v[1]+v[2] << endl;
+    cout << v[0] << "\n" << v[0] + v[1] + v[2];
 }
-
-
