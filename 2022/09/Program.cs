9998a7eea35e3b0ba9de7b942a4c0712e826decf
magyarakoos <ninjaoffical08@gmail.com>
2023-11-20 19:13:25 +0100

    Falied to solve 2022/09, had to delete its directory

--- 2022/09/Program.cs
+++
@@ -1,4 +0,0 @@
-string[] input = File.ReadAllLines("input.txt");
-(char[] dir, byte[] cnt) = (input.Select(l => l[0]).ToArray(), input.Select(l => byte.Parse(l.Split(' ')[1])).ToArray());
-
-// TODO: solve the problem
