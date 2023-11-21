58a6a9f022cf02f6e012eb04c02aad5c4480fc29
LordOfPumpkins <ninjaoffical08@gmail.com>
2023-11-21 20:28:14 +0100

    Add 2022/04 in C#

---
+++ 2022/04/Program.cs
@@ -0,0 +1,8 @@
+﻿var input = File.ReadAllLines("input").Select(l => l.Split(new char[]{',', '-'}).Select(int.Parse).ToArray());
+var p1 = 0;
+var p2 = 0;
+foreach (var a in input) {
+    if ((a[0] <= a[2] && a[1] >= a[3]) || (a[0] >= a[2] && a[1] <= a[3])) p1++; 
+    if ((a[0] <= a[2] && a[1] >= a[2]) || (a[0] >= a[2] && a[1] >= a[2])) p2++;
+}
+Console.WriteLine(p1 + "\n" + p2);
