2791719e2b9829b4445b305ef80d9fcd3ec68513
LordOfPumpkins <ninjaoffical08@gmail.com>
2023-12-11 13:18:22 +0100

    Solve 2023/11

--- 2023/11/main.cpp
+++ 2023/11/main.cpp
@@ -9,20 +9,16 @@ int main() {
     fstream f("input");
     string s;
     vector<string> v;
+    vector<int> gapsX, gapsY;
     while (f >> s) {
         v.push_back(s);
         if (all_of(s.begin(), s.end(), [](char c){ return c == '.'; })) {
-            v.push_back(s);
+            gapsX.push_back(v.size() - 1);
         }
     }
     for (int i = 0, j; i < v[0].size(); i++) {
         for (j = 0; j < v.size() && v[j][i] == '.'; j++);
-        if (j == v.size()) {
-            for (int k = 0; k < v.size(); k++) {
-                v[k].insert(v[k].begin() + i, '.');
-            }
-            i++;
-        }
+        if (j == v.size()) gapsY.push_back(i);
     }
     vector<point> px;
     for (int i = 0; i < v.size(); i++) {
@@ -30,11 +26,23 @@ int main() {
             if (v[i][j] == '#') px.push_back({i, j});
         }
     }
-    int p1 = 0;
+    long long p1 = 0, p2 = 0;
     for (int i = 0; i < px.size(); i++) {
         for (int j = i + 1; j < px.size(); j++) {
-            p1 += abs(px[i].x - px[j].x) + abs(px[i].y - px[j].y);
+            long long n = 0, dist = abs(px[i].x - px[j].x) + abs(px[i].y - px[j].y);
+            for (int g : gapsX) {
+                if (min(px[i].x, px[j].x) < g && max(px[i].x, px[j].x) > g) {
+                    n++;
+                }
+            }
+            for (int g : gapsY) {
+                if (min(px[i].y, px[j].y) < g && max(px[i].y, px[j].y) > g) {
+                    n++;
+                }
+            }
+            p1 += dist + n;
+            p2 += abs(px[i].x - px[j].x) + abs(px[i].y - px[j].y) + n * (1e6 - 1);
         }
     }
-    cout << p1;
+    cout << p1 << '\n' << p2;
 }
