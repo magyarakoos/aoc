4ac46482fb060100323823cfce968a863937e4b2
LordOfPumpkins <ninjaoffical08@gmail.com>
2023-12-16 18:17:45 +0100

    Refactor 2023/16

--- 2023/16/main.cpp
+++ 2023/16/main.cpp
@@ -1,59 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using point = pair<int, int>;
-#define x first
-#define y second
-
+using point = array<int, 2>;
 vector<string> grid;
 int N, M;
 
-void bounce(point p, point dir, vector<vector<pair<bool, point>>>& vis) {
-    auto& v = vis[p.y][p.x];
-    if (p.y < 0 || p.y >= N || p.x < 0 || p.x >= M || (v.x && dir == v.y)) return;
-    if (!v.x) v.y = dir;
-    v.x = 1;
-    char c = grid[p.y][p.x];
-    if (c == '-' && !dir.x) {
-        bounce({p.x - 1, p.y}, {-1, 0}, vis);
-        bounce({p.x + 1, p.y}, {1, 0}, vis);
+void bounce(point p, point dir, vector<vector<point>>& vis) {
+    point& v = vis[p[1]][p[0]];
+    if (p[1] < 0 || p[1] >= N || p[0] < 0 || p[0] >= M || dir == v) return;
+    if (!(v[0] + v[1])) v = dir;
+    char c = grid[p[1]][p[0]];
+    if (c == '-' && !dir[0]) {
+        bounce({p[0] - 1, p[1]}, {-1, 0}, vis);
+        bounce({p[0] + 1, p[1]}, {1, 0}, vis);
         return;
-    } else if (c == '|' && !dir.y) {
-        bounce({p.x, p.y - 1}, {0, -1}, vis);
-        bounce({p.x, p.y + 1}, {0, 1}, vis);
+    } else if (c == '|' && !dir[1]) {
+        bounce({p[0], p[1] - 1}, {0, -1}, vis);
+        bounce({p[0], p[1] + 1}, {0, 1}, vis);
         return;
     } else if (c == '/') {
-        dir = {-dir.y, -dir.x};
+        dir = {-dir[1], -dir[0]};
     } else if (c == '\\') {
-        dir = {dir.y, dir.x};
+        swap(dir[0], dir[1]);
     }
-    bounce({p.x + dir.x, p.y + dir.y}, dir, vis);
+    bounce({p[0] + dir[0], p[1] + dir[1]}, dir, vis);
 }
 
 int solve(point p, point dir) {
-    vector<vector<pair<bool, point>>> vis(N, vector<pair<bool, point>>(M));
+    vector<vector<point>> vis(N, vector<point>(M, {0, 0}));
     bounce(p, dir, vis);
     int result = 0;
-    for (const auto& l : vis) for (auto& b : l) result += b.x;
+    for (const auto& l : vis) for (const point& p : l) result += (bool)(p[0] + p[1]);
     return result;
 }
 
 int main() {
     fstream f("input");
     string s;
-    while (f >> s) {
-        grid.push_back(s);
-    }
-    N = grid.size();
-    M = grid[0].size();
+    while (f >> s) grid.push_back(s);
+    N = grid.size(), M = grid[0].size();
     int p1 = solve({0, 0}, {1, 0}), p2 = p1;
     for (int i = 1; i < N; i++) {
-        p2 = max(p2, solve({0, i}, {1, 0}));
-        p2 = max(p2, solve({M - 1, i}, {-1, 0}));
+        p2 = max({p2, solve({0, i}, {1, 0}), solve({M - 1, i}, {-1, 0})});
     }
     for (int i = 0; i < M; i++) {
-        p2 = max(p2, solve({i, 0}, {0, 1}));
-        p2 = max(p2, solve({i, N - 1}, {0, -1}));
+        p2 = max({p2, solve({i, 0}, {0, 1}), solve({i, N - 1}, {0, -1})});
     }
     cout << p1 << '\n' << p2;
 }
