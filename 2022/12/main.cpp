6e64add5338bf70c0fa7bb28de52d2a97fe43682
magyarakoos <ninjaoffical08@gmail.com>
2024-02-22 18:59:06 +0100

    arch descktop sync: 2024-02-22 18:59:06

--- 2022/12/main.cpp
+++ 2022/12/main.cpp
@@ -13,37 +13,13 @@ int main() {
 
     point sp, ep;
     
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
+    for (int i = 0; i < size(grid); i++) {
+        for (int j = 0; j < grid[0].size(); j++) {
             if (grid[i][j] == 'S') {
                 sp = {i, j};
             }
             if (grid[i][j] == 'E') {
                 ep = {i, j};
-            }
-        }
-    }
-    
-    set<point> vis({sp});
-    queue<array<int, 3>> q({{sp[0], sp[1], 0}});
-
-    while (q.empty()) {
-        auto [y, x, steps] = q.front(); q.pop();
-
-        if (y == ep[0] && x == ep[1]) {
-            cout << steps;
-            exit(0);
-        }
-
-        for (point dir : dirs) {
-            point np = {dir[0] + y, dir[1] + x};
-            if (np[0] < 0 || np[1] < 0 || np[0] >= N || np[1] >= M) {
-                continue;
-            }
-
-            if (grid[y][x] - grid[np[0]][np[1]] >= -1 && !vis.count(np)) {
-                vis.insert(np);
-                q.push({np[0], np[1], steps + 1});
             }
         }
     }
