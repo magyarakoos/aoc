58f3366eab0f490fb5c03020e7808904659961bf
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-26 10:16:15 +0100

    arch descktop sync: 2024-02-26 10:16:15

--- 2022/14/main.cpp
+++ 2022/14/main.cpp
@@ -57,6 +57,7 @@ int main() {
     while (drop(500, 0)) {
         cout << "\x1B[2J\x1B[H";
         for (const string& line : grid) cout << line << '\n';
+        sleep(1);
     }
     cout << p1;
 }
