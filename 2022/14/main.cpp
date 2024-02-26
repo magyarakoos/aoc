b1319c21d6b3c8c094d81763c45ecb1d5a722031
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-26 09:37:31 +0100

    arch descktop sync: 2024-02-26 09:37:31

--- 2022/14/main.cpp
+++ 2022/14/main.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+using point = array<int, 2>;
 
 int main() {
     ifstream f("input");
     string s;
     while (getline(f, s)) {
-        
+
     }
 }
