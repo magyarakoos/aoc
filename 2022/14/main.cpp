771b5e44ab3bee9d1ecc0b11fdcfa9522c91276a
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-26 10:18:49 +0100

    arch descktop sync: 2024-02-26 10:18:49

--- 2022/14/main.cpp
+++ 2022/14/main.cpp
@@ -7,13 +7,13 @@ vector<string> grid;
 
 bool drop(int x, int y) {
 
-    cout << x << ' ' << y << '\n';
 
     if (y + 1 == N) return 0;
     if (grid[y + 1][x] == '.') return drop(x, y + 1);
     else if (grid[y + 1][x - 1] == '.') return drop(x - 1, y + 1);
     else if (grid[y + 1][x + 1] == '.') return drop(x + 1, y + 1);
     grid[y][x] = 'o';
+    cout << x << ' ' << y << '\n';
     return 1;
 }
 
