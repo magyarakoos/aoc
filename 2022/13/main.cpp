e58a66e21479c729cc7386d470b0ebd14f2492ca
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-26 09:25:10 +0100

    arch descktop sync: 2024-02-26 09:25:10

--- 2022/13/main.cpp
+++ 2022/13/main.cpp
@@ -16,8 +16,12 @@ vector<int> tokenize(string& s) {
 int main() {
     ifstream f("input");
     string s, t;
+    int p1 = 0;
     while (f >> s >> t) {
         vector<int> sv = tokenize(s), tv = tokenize(t);
-        if (sv)
+        if (sv.size() < tv.size()) {
+            for ()
+            p1++;
+        }
     }
 }
