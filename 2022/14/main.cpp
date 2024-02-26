561e041e9c13756b094c9046f1e6e3ae6fd20c76
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-26 10:00:04 +0100

    arch descktop sync: 2024-02-26 10:00:04

--- 2022/14/main.cpp
+++ 2022/14/main.cpp
@@ -40,10 +40,13 @@ int main() {
         for (size_t i = 1; i < worm.size(); i++) {
             for (int y = min(worm[i - 1][1], worm[i][1]); y <= max(worm[i - 1][1], worm[i][1]); y++) {
                 for (int x = min(worm[i - 1][0], worm[i][0]); x <= max(worm[i - 1][0], worm[i][0]); x++) {
+                    grid[y][x - minx] = '#';
                     cout << x - minx << ' ' << y << '\n';
                 }
             }
             cout << '\n';
         }
     }
+
+    grid[0][500 - minx] = '+';
 }
