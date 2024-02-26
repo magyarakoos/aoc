2f0bad20c3b88b0f73ba60fd9614971288388efb
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-26 09:21:45 +0100

    arch descktop sync: 2024-02-26 09:21:45

--- 2022/13/main.cpp
+++ 2022/13/main.cpp
@@ -5,6 +5,10 @@ int main() {
     ifstream f("input");
     string s, t;
     while (f >> s >> t) {
-        
+        for (char& c : s) {
+            if (c == '[' || c == ']' || c == ',') {
+                c = 
+            }
+        }
     }
 }
