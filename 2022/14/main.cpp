42c2d801c0c8375e621e945a87625541f8d079f8
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-26 09:53:00 +0100

    arch descktop sync: 2024-02-26 09:53:00

--- 2022/14/main.cpp
+++ 2022/14/main.cpp
@@ -30,7 +30,7 @@ int main() {
     }
 
     int N = maxy - miny + 1, M = maxx - minx + 1;
-    vector<vector<char>> grid(N, vector<char>(M));
+    vector<string> grid(N, string(M, '.'));
 
     for (const auto& worm : v) {
         for (int i = 1; i < worm.size(); i++) {
