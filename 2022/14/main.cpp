8ebfceeb1d8172bd23ad607eed56a9df82f11988
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-26 14:28:39 +0100

    arch descktop sync: 2024-02-26 14:28:39

--- 2022/14/main.cpp
+++ 2022/14/main.cpp
@@ -58,8 +58,7 @@ int main() {
         cout << "\x1B[2J\x1B[H";
         for (const string& line : grid) cout << line << '\n';
         cout << grid[0][500 - minx] << '\n';
-        this_thread::sleep_for(chrono::milliseconds(250));
-
+        this_thread::sleep_for(chrono::milliseconds(100));
         drop(500 - minx, 0);
         p2++;
         if (grid[0][500 - minx] == 'o') break;
