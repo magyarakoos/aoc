33d7f1e5f855ec6669858da9700c233d516bbc2b
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-26 09:52:31 +0100

    arch descktop sync: 2024-02-26 09:52:31

--- 2022/14/main.cpp
+++ 2022/14/main.cpp
@@ -35,7 +35,9 @@ int main() {
     for (const auto& worm : v) {
         for (int i = 1; i < worm.size(); i++) {
             for (int y = min(worm[i - 1][1], worm[i][1]); y <= max(worm[i - 1][1], worm[i][1]); y++) {
-                for (int x = min(worm[i - 1][0], worm[i][0]); x <= max(worm[i - 1][1]))
+                for (int x = min(worm[i - 1][0], worm[i][0]); x <= max(worm[i - 1][0], worm[i][0]); x++) {
+                    grid[y][x] = '#';
+                }
             }
         }
     }    
