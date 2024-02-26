bf01ac4c8a6b1000f9720f0896f25738da76f42d
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-26 14:01:20 +0100

    arch descktop sync: 2024-02-26 14:01:20

--- 2022/14/main.cpp
+++ 2022/14/main.cpp
@@ -38,7 +38,7 @@ int main() {
         }
     }
     maxy++; minx--; maxx++;
-    N = maxy + 2, M = maxx - minx + 1;
+    N = maxy + 2, M = maxx - minx + 1 + 1000;
     grid.assign(N, string(M, '.'));
     for (const auto& worm : v) {
         for (size_t i = 1; i < worm.size(); i++) {
@@ -52,5 +52,7 @@ int main() {
 
     for (const string& line : grid) cout << line << '\n';
 
-    replace()
+    replace(grid.back().begin(), grid.back().end(), '.', '#');
+
+
 }
