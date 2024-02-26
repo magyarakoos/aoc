7b2e836834ed7d5ac99d3974776e1369c2401dd4
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-26 09:26:34 +0100

    arch descktop sync: 2024-02-26 09:26:34

--- 2022/13/main.cpp
+++ 2022/13/main.cpp
@@ -1,17 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct token {
+    union {
+        vector<token> list;
+        int x;
+    };
+}
+
 int main() {
     ifstream f("input");
     string s, t;
     int p1 = 0;
     while (f >> s >> t) {
-        vector<int> sv = tokenize(s), tv = tokenize(t);
-        if (sv.size() < tv.size()) {
-            for (int i = 0; i < sv.size(); i++) {
-                if (sv)
-            }
-            p1++;
-        }
+        
     }
 }
