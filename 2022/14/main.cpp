c8ea511d3ea6cfe4efede4300d00e7940c0e7534
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-26 10:20:58 +0100

    arch descktop sync: 2024-02-26 10:20:58

--- 2022/14/main.cpp
+++ 2022/14/main.cpp
@@ -8,8 +8,6 @@ vector<string> grid;
 bool drop(int x, int y) {
 
     cout << x << ' ' << y << ' ';
-    cout << grid[y + 1][x] << ' ' << grid[y + 1][x - 1] << ' ' << grid[y + 1][x + 1] << '\n';
-
     if (y + 1 == N) return 0;
     if (grid[y + 1][x] == '.') return drop(x, y + 1);
     else if (grid[y + 1][x - 1] == '.') return drop(x - 1, y + 1);
@@ -59,7 +57,7 @@ int main() {
     }
 
     int p1 = 0;
-    while (drop(500, 0)) {
+    while (drop(500 - minx, 0)) {
         /*cout << "\x1B[2J\x1B[H";
         for (const string& line : grid) cout << line << '\n';
         cout << p1 << '\n';*/
