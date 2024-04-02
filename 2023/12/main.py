457cdabfa026c5732fb753c102b20a15ac455dc8
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-02 16:34:29 +0200

    arch descktop sync: 2024-04-02 16:34:29

--- 2023/12/main.py
+++ 2023/12/main.py
@@ -87,7 +87,7 @@ def ways(s, target_runs):
 
     for i in range(n):
         for j in range(m):
-            for k in range(target_runs[j]+1):
+            for k in range():
                 print(dp[i][j][k],end="")
                 print(" ")
             print()
