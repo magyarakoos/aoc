d1159bc88e873428451705cc5178bbab79e90ad4
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-02 12:07:00 +0200

    arch descktop sync: 2024-04-02 12:07:00

--- 2023/06/quad2.cpp
+++ 2023/06/quad2.cpp
@@ -15,5 +15,5 @@ int main() {
         x2 = (t + sqrt(t * t - 4 * d)) / 2;
     x1 = ceil(x1)  + (x1 == floor(x1));
     x2 = floor(x2) - (x2 == floor(x2));
-    cout << x2 - x1 + 1;
+    cout << x1 << " " << x2 << "\n";
 }
