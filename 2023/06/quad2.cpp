f717d1ec5c8054ef92726f3e20bbd423fd3fcfac
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-02 12:16:52 +0200

    arch descktop sync: 2024-04-02 12:16:52

--- 2023/06/quad2.cpp
+++ 2023/06/quad2.cpp
@@ -11,7 +11,7 @@ int main() {
     double d = stold(r), t = stold(s);
     cout << d << " " << t << "\n";
     double 
-        x1 = (t - sqrt(t * t - 4 * d)) / 2,
+        x1 = (t - sqrt(pow(t, 2) - 4 * d)) / 2,
         x2 = (t + sqrt(t * t - 4 * d)) / 2;
     cout << x1 << " " << x2 << "\n";
     x1 = ceil(x1)  + (x1 == floor(x1));
