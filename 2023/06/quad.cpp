e07f386ff4475705decebe9b67a637d179e46c5c
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-02 11:32:49 +0200

    arch descktop sync: 2024-04-02 11:32:49

--- 2023/06/quad.cpp
+++ 2023/06/quad.cpp
@@ -10,7 +10,7 @@ int main() {
     while (f >> s) d.push_back(stoi(s));
     int p1 = 1;
     for (int i = 0; i < t.size(); i++) {
-        float x1 = -t[i] - sqrt(t[i]*t[i]-)
+        float x1 = -t[i] - sqrt(t[i]*t[i]-4*)
     }
     cout << p1;
 }
