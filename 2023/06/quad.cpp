6836c96306b358d41de27136e53ec21978ed9653
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-02 11:35:17 +0200

    arch descktop sync: 2024-04-02 11:35:17

--- 2023/06/quad.cpp
+++ 2023/06/quad.cpp
@@ -5,7 +5,7 @@ int main() {
     ifstream f("input");
     string s;
     f >> s;
-    vector<int> t({0}), d({0});
+    vector<int> t, d;
     while (f >> s && s[0] != 'D') t.push_back(stoi(s));
     while (f >> s) d.push_back(stoi(s));
     int p1 = 1;
