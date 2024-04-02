b9857d454d83c07210e3255c06c708e490484aa3
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-02 16:21:58 +0200

    arch descktop sync: 2024-04-02 16:21:58

--- 2023/12/part2.cpp
+++ 2023/12/part2.cpp
@@ -45,9 +45,11 @@ ll solve(const string& s, const vector<int>& v) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             for (int k = 0; k < O; k++) {
-                cout << dp[i][j]
+                cout << dp[i][j][k] << " ";
             }
+            cout << "\n";
         }
+        cout << "\n";
     }
 
     return dp.back().back().front();
