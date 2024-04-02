55922d9bab292a5881bb6bd050b531d0bbba3d0b
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-02 12:03:58 +0200

    arch descktop sync: 2024-04-02 12:03:58

--- 2023/06/quad2.cpp
+++ 2023/06/quad2.cpp
@@ -9,5 +9,10 @@ int main() {
     r.erase(remove_if(r.begin(), r.end(), ::isspace), r.end());
     s.erase(remove_if(s.begin(), s.end(), ::isspace), s.end());
     long long d = stoll(r), t = stoll(s);
-    
+    double 
+        x1 = (t - sqrt(t * t - 4 * d)) / 2,
+        x2 = (t + sqrt(t * t - 4 * d)) / 2;
+    x1 = ceil(x1)  + (x1 == floor(x1));
+    x2 = ;
+    cout << x2 - x1 + 1;
 }
