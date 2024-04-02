3134b8a873ce06492d462bda32071b78e085102a
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-02 11:45:13 +0200

    arch descktop sync: 2024-04-02 11:45:13

--- 2023/06/quad.cpp
+++ 2023/06/quad.cpp
@@ -8,17 +8,16 @@ int main() {
     vector<int> t, d;
     while (f >> s && s[0] != 'D') t.push_back(stoi(s));
     while (f >> s) d.push_back(stoi(s));
-    int p1 = 1;
-    for (int j = 0; j < 10000; j++) {
-
-        for (int i = 0; i < t.size(); i++) {
-            double 
-                x1 = (t[i] - sqrt(t[i] * t[i] - 4 * d[i])) / 2,
-                x2 = (t[i] + sqrt(t[i] * t[i] - 4 * d[i])) / 2;
-            x1 = ceil(x1)  + (x1 == floor(x1));
-            x2 = floor(x2) - (x2 == floor(x2));
-            p1 *= x2 - x1 + 1;
-        }
+    for (int j = 0; j < 100000; j++) {
+    for (int i = 0; i < t.size(); i++) {
+        int p1 = 1;
+        double 
+            x1 = (t[i] - sqrt(t[i] * t[i] - 4 * d[i])) / 2,
+            x2 = (t[i] + sqrt(t[i] * t[i] - 4 * d[i])) / 2;
+        x1 = ceil(x1)  + (x1 == floor(x1));
+        x2 = floor(x2) - (x2 == floor(x2));
+        p1 *= x2 - x1 + 1;
+        cout << p1;
     }
-    cout << p1;
+    }
 }
