538e53fd30af47fc7f76b3b5d65435a86b4d5049
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-02 15:56:51 +0200

    arch descktop sync: 2024-04-02 15:56:51

--- 2023/12/part2.cpp
+++ 2023/12/part2.cpp
@@ -10,11 +10,10 @@ ll solve(const string& s, const vector<int>& v) {
 
     vector<vector<vector<ll>>> dp(N, vector<vector<ll>>(M, vector<ll>(O)));
 
-    for (int j = 0; j < M; j++) {
     for (int k = 0; k < O; k++) {
 
-        
-    }}
+
+    }
 
     for (int i = 1; i < N; i++) {
     for (int j = 0; j < M; j++) {
