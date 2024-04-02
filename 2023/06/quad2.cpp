f48dcec3e69ec24a334bb8cfd904bc697c74b949
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-02 12:02:12 +0200

    arch descktop sync: 2024-04-02 12:02:12

--- 2023/06/quad2.cpp
+++ 2023/06/quad2.cpp
@@ -6,7 +6,8 @@ int main() {
     string r, s;
     f >> r; getline(f, r);
     f >> s; getline(f, s);
+    r.erase(remove_if(r.begin(), r.end(), ::isspace), r.end());
     s.erase(remove_if(s.begin(), s.end(), ::isspace), s.end());
-    long long d = stoll(s);
+    long long d = stoll(t), t = stoll(s);
     cout << d;
 }
