35e189ccb7dc3813a0416fa481ea0ab6bb6ecb8f
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-02 11:28:04 +0200

    arch descktop sync: 2024-04-02 11:28:04

--- 2023/06/quad.cpp
+++ 2023/06/quad.cpp
@@ -6,7 +6,7 @@ int main() {
     string s;
     f >> s;
     vector<int> t({0}), d({0});
-    while (f >> s && s[0] != '') {
-        t.push_back(0);
-    }
+    while (f >> s && s[0] != 'D') t.push_back(stoi(s));
+    while (f >> s) d.push_back(stoi(s));
+    
 }
