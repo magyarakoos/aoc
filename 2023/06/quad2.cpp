41fb836d1a143ea1c3a4bef8ca0621bea8937c47
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-02 11:59:16 +0200

    arch descktop sync: 2024-04-02 11:59:16

--- 2023/06/quad2.cpp
+++ 2023/06/quad2.cpp
@@ -3,9 +3,7 @@ using namespace std;
 
 int main() {
     ifstream f("input");
-    string r, s;
-    f >> r; getline(f, r);
-    f >> s; getline(f, s);
-    long long t = stoll(istringstream(r) >> std::ws);
+    string s;
+    getline(f, s);
     long long d = stoll(istringstream(s) >> std::ws);
 }
