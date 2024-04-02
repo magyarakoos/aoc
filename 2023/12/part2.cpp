435a50ec02c1dbc51c3fe87a6989d7108a6bf715
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-02 15:45:44 +0200

    arch descktop sync: 2024-04-02 15:45:44

--- 2023/12/part2.cpp
+++ 2023/12/part2.cpp
@@ -11,12 +11,9 @@ int main() {
     string s, t;
     ll p2 = 0;
     while (f >> s >> t) {
-        replace(t.begin(), t.end(), ',', ' ');
-        stringstream ss(t);
-        int n;
-        vector<int> v, w;
-        while (ss >> n) v.push_back(n);
-        for (int i = 0; i < 5;)
-        p2 += solve(s, {});
+        
+        for (int i = 0; i < 5; i++) {
+
+        }
     }
 }
