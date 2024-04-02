8000e8b470b986dc383146d25b935d773ec2cc1b
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-02 12:20:20 +0200

    arch descktop sync: 2024-04-02 12:20:20

--- 2023/06/quad2.cpp
+++ 2023/06/quad2.cpp
@@ -8,13 +8,10 @@ int main() {
     f >> s; getline(f, s);
     r.erase(remove_if(r.begin(), r.end(), ::isspace), r.end());
     s.erase(remove_if(s.begin(), s.end(), ::isspace), s.end());
-    double t = stold(r), t = stold(s);
-    cout << d << " " << t << "\n";
+    double  d = stold(s);
     double 
         x1 = (t - sqrt(pow(t, 2) - 4 * d)) / 2,
         x2 = (t + sqrt(pow(t, 2) - 4 * d)) / 2;
-    cout << x1 << " " << x2 << "\n";
     x1 = ceil(x1)  + (x1 == floor(x1));
     x2 = floor(x2) - (x2 == floor(x2));
-    cout << x1 << " " << x2 << "\n";
 }
