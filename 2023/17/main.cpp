9f57dfb777287d820e898caf7f40c3e7ee78a24f
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-02 13:19:23 +0200

    arch descktop sync: 2024-04-02 13:19:23

--- 2023/17/main.cpp
+++ 2023/17/main.cpp
@@ -29,8 +29,7 @@ int dijkstra() {
 
             int nx = x + dx, ny = y + dy;
             if (nx < 0 || ny < 0 || nx >= M || ny >= N ||
-                dir == (i + 2) % 4)
-                 continue;
+                dir == (i + 2) % 4) continue;
 
             int ndist = dist + g[ny][nx];
             if (distS[ny][nx] > ndist) {
