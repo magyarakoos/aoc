4584a78510f5ea7e39c9f3400acebc1c2eb04ac2
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-02 11:48:44 +0200

    arch descktop sync: 2024-04-02 11:48:44

--- 2023/06/quad.cpp
+++ 2023/06/quad.cpp
@@ -8,8 +8,8 @@ int main() {
     vector<int> t, d;
     while (f >> s && s[0] != 'D') t.push_back(stoi(s));
     while (f >> s) d.push_back(stoi(s));
+    int p1 = 1;
     for (int i = 0; i < t.size(); i++) {
-        int p1 = 1;
         double 
             x1 = (t[i] - sqrt(t[i] * t[i] - 4 * d[i])) / 2,
             x2 = (t[i] + sqrt(t[i] * t[i] - 4 * d[i])) / 2;
