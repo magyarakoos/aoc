e26d7fe79361fd38b4b2af8ed78f99550a29725c
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 10:59:17 +0200

    arch descktop sync: 2024-04-13 10:59:17

--- 2023/08/part1.jl
+++ 2023/08/part1.jl
@@ -5,6 +5,7 @@ g = Dict()
 for l in eachline(f)
     g[l[1:3]] = (l[8:10], l[13:15])
 end
+(p1 = 0; p2 = 0)
 (i = 1; s = "AAA")
 while (s != "ZZZ")
     global s = g[s][inst[i % (length(inst) + 1)] == 'L' ? 1 : 2]
