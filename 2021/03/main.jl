8f9b5f84d43a3104be5d43a898aeee9dff3c158a
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 23:36:02 +0200

    arch descktop sync: 2024-04-13 23:36:02

--- 2021/03/main.jl
+++ 2021/03/main.jl
@@ -1,6 +1,8 @@
 a = [parse.(Int, split(l, "")) for l in eachline("input")]
 zc = Vector{Int}(length(a)); oc = zc
 for j in eachindex(a[1]), i in eachindex(a)
-    if (a[i][j] == 1)
-        
+    if (a[i][j] == 1) global oc[j] += 1
+    else gloal zc[j] += 1 end
 end
+println(zc)
+println(oc)
