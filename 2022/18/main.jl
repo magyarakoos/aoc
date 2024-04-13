e36f021d7f3c7f7a8398635b059649a2d9ed4d6a
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 19:28:30 +0200

    arch descktop sync: 2024-04-13 19:28:30

--- 2022/18/main.jl
+++ 2022/18/main.jl
@@ -1,5 +1,5 @@
 σ = Set()
-◿ = () -> maximum(map(ρ -> maximum(ρ), σ))
+◿ = () -> map(ρ -> maximum(ρ), σ)
 λ = (m) -> push!(σ, (m[1], m[2], m[3]))
 Ψ = (x, y, z) -> [(x+1, y, z), (x-1, y, z), (x, y+1, z), (x, y-1, z), (x, y, z+1), (x, y, z-1)]
 map(l -> λ(map(x -> parse(Int, x), match(r"(\d+),(\d+),(\d+)", l))), readlines("input"))
@@ -10,3 +10,4 @@ for (x, y, z) in σ, ρ in Ψ(x, y, z)
     end
 end
 println(Σ)
+println(◿)
