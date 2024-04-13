5db2a17e8caa5fdd97a240b3654f05976d50dd29
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 13:43:22 +0200

    arch descktop sync: 2024-04-13 13:43:22

--- 2022/16/part1.jl
+++ 2022/16/part1.jl
@@ -1,5 +1,10 @@
+# f = map(
+#     map(a -> (a[1], parse.(a[2]), split(a[3:end], ", ")), 
+#         l -> match(r".{6}(\w+).{15}(\d+).{23}\w?\s(.*)",l)), 
+#     open("input", "r")
+# )
+
 f = map(
-    map(a -> (a[1], parse.(a[2]), split(a[3:end], ", ")), 
-        l -> match(r".{6}(\w+).{15}(\d+).{23}\w?\s(.*)",l)), 
+    map(a -> match(r".{6}(\w+).{15}(\d+).{23}\w?\s(.*)",l)), 
     open("input", "r")
 )
