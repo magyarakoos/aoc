909abc61a4c0b53a545cf7dadf9b4bc84ab4a67e
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 23:33:44 +0200

    arch descktop sync: 2024-04-13 23:33:44

--- 2021/03/main.jl
+++ 2021/03/main.jl
@@ -1,4 +1,5 @@
 a = [parse.(Int, split(l, "")) for l in eachline("input")]
-res = Array{Array{Int}(2)}(length(a))
+res = Vector{Vector{Int}}(undef, length(a))
 for j in eachindex(a[1]), i in eachindex(a)
+    res[]
 end
