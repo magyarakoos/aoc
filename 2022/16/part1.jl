701b6306f230361282cd5bc1277effad4deb1419
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 17:24:15 +0200

    arch descktop sync: 2024-04-13 17:24:15

--- 2022/16/part1.jl
+++ 2022/16/part1.jl
@@ -1,8 +1,5 @@
 
-f = map(a -> [
-        string(a[1]), 
-        parse(Int, a[2]), 
-        map(s -> string(s), split(a[3], ", "))], 
+f = map(a -> [a[1], parse(Int, a[2]), split(a[3], ", ")], 
     map(l -> 
         match(r".{6}(\w+).{15}(\d+).{23}\w?\s(.*)", l), 
         readlines("input")
