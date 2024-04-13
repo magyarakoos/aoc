08283b6bf023af7ddea4bc70bad66f86f90699c5
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 15:36:00 +0200

    arch descktop sync: 2024-04-13 15:36:00

--- 2022/16/main.jl
+++ 2022/16/main.jl
@@ -1,13 +1,3 @@
-function issub(t, s)
-    i, j = 1, 1
-    while i <= length(s) && j <= length(t)
-        if s[i] == t[j]
-            i += 1
-        end
-        j += 1
-    end
-    return i > length(s)
-end
 s = readline()
 t = lowercase(readline())
 if (t[3] == 'x')
