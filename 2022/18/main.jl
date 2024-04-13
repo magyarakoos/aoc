23af9dfaf2f48a6baeccb9389ebaeeae43fd8d24
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 19:03:49 +0200

    arch descktop sync: 2024-04-13 19:03:49

--- 2022/18/main.jl
+++ 2022/18/main.jl
@@ -1,5 +1,5 @@
 s = Set()
-f = (l) -> push!(s, )
+f = (l) -> push!(s, (parse(l[1]), parse(l[2]), parse(l[3])))
 for p in map(l -> (parse(l[1]), parse(l[2]), parse(l[3])), readlines("input"))
-    push!(s,)
+    push!(s, p)
 end
