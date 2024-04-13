ec2bb6189a70415ec58e3e07884f5e068816e136
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 11:09:05 +0200

    arch descktop sync: 2024-04-13 11:09:05

--- 2023/08/part1.jl
+++ 2023/08/part1.jl
@@ -5,13 +5,9 @@ g = Dict()
 for l in eachline(f)
     g[l[1:3]] = (l[8:10], l[13:15])
 end
-i = 
-for s in starts
-    i = 0
-    while (s[3] != 'Z')
-        s = g[s][inst[(i % length(inst)) + 1] == 'L' ? 1 : 2]
-        i += 1
-    end
-    global p2 = lcm(p2, i)
+i = 0; s = "AAA"
+while (s != "ZZZ")
+    s = g[s][inst[(i % length(inst)) + 1] == 'L' ? 1 : 2]
+    i += 1
 end
-println(p2)
+println(i)
