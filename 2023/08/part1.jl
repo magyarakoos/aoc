68c55ed3fb79bfd076b2b51199bae710bd0d04dc
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 10:45:29 +0200

    arch descktop sync: 2024-04-13 10:45:29

--- 2023/08/part1.jl
+++ 2023/08/part1.jl
@@ -1,21 +1,8 @@
-function wander(curr, i, inst, g)
-    if (curr == "ZZZ")
-        return 0
-    end
-    if (inst[i % (length(inst) + 1)] == 'L')
-        return wander(g[curr][0], i + 1, inst, g)
-    else
-        return wander(g[curr][1], i + 1, inst, g)
-    end
-end
-
 f = open("input", "r")
 inst = readline(f)
 readline(f)
 g = Dict()
 for l in eachline(f)
     m = match(r"(\w+) = \((\w+), (\w+)\)", l)
-    println(m[1])
+    g[m[1]] = (m[2], m[3])
 end
-
-//println(wander("AAA", 1, inst, g))
