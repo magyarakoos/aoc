189f6e8ac5ddefb89becf9f1786059218fb9766d
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 11:52:44 +0200

    arch descktop sync: 2024-04-13 11:52:44

--- 2022/15/part1.jl
+++ 2022/15/part1.jl
@@ -2,5 +2,8 @@ rx = r".{12}(-?\d+).{4}(-?\d+).{25}(-?\d+).{4}(-?\d+)"
 
 for l in eachline(open("input", "r"))
     # println(l)
-    for s in println(match(rx, l))
+    for s in match(rx, l)
+        println(s)
+    end
+    println()
 end
