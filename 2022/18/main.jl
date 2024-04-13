4408d68026161f99bb2b4745f999db1770a05457
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 19:21:05 +0200

    arch descktop sync: 2024-04-13 19:21:05

--- 2022/18/main.jl
+++ 2022/18/main.jl
@@ -1,11 +1,10 @@
 σ = Set()
-λ = (m) -> push!(σ, [m[1], m[2], m[3]])
-Ψ = (x, y, z) -> [[x+1, y, z], [x-1, y, z], [x, y+1, z], [x, y-1, z], [x, y, z+1], [x, y, z-1]]
+λ = (m) -> push!(σ, (m[1], m[2], m[3]))
+Ψ = (x, y, z) -> [(x+1, y, z), (x-1, y, z), (x, y+1, z), (x, y-1, z), (x, y, z+1), (x, y, z-1)]
 map(l -> λ(map(x -> parse(Int, x), match(r"(\d+),(\d+),(\d+)", l))), readlines("input"))
 Σ = length(σ) * 6
 for (x, y, z) in σ, ρ in Ψ(x, y, z)
-    
-    if ∈(σ, ρ)
+    if ∈(ρ, σ)
         global Σ -= 1
     end
 end
