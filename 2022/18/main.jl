c639d462303dfb71771e3a74f4bc9ee0e2571fa9
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 19:04:24 +0200

    arch descktop sync: 2024-04-13 19:04:24

--- 2022/18/main.jl
+++ 2022/18/main.jl
@@ -1,5 +1,1 @@
-s = Set()
-f = (l) -> push!(s, (parse(l[1]), parse(l[2]), parse(l[3])))
-for p in map(l -> (parse(l[1]), parse(l[2]), parse(l[3])), readlines("input"))
-    push!(s, p)
-end
+match()
