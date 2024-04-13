bdcda1bffa839da8512dffd2a7b992c2a3781bbb
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 11:57:20 +0200

    arch descktop sync: 2024-04-13 11:57:20

--- 2022/15/part1.jl
+++ 2022/15/part1.jl
@@ -4,7 +4,6 @@ rx = r".{12}(-?\d+).{4}(-?\d+).{25}(-?\d+).{4}(-?\d+)"
 for l in eachline(open("input", "r"))
     m = [parse.(Int, match(rx, l)[i]) for i in 1:4]
     ps = (m[1], m[2]); pb = (m[3], m[4])
-    println((ps, pb))
-    ∆(ps, pb)
+    println(∆(ps, pb))
 
 end
