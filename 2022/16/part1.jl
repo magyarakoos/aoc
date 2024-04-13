e6dd4aa5a95c13bdf2084a568520d18a996633de
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 13:52:02 +0200

    arch descktop sync: 2024-04-13 13:52:02

--- 2022/16/part1.jl
+++ 2022/16/part1.jl
@@ -1,5 +1,4 @@
-f = map(l -> match(r".{6}(\w+).{15}(\d+).{23}\w?\s(.*)", l), 
-    readlines("input"))
-
-g = map(l -> [l[1], parse.(Int, l[2]), split(l[3], ", ")], f)
-println(g)
+f = map(a -> [a[1], parse.(Int, a[2]), split(a[3], ", ")], 
+    map(l -> 
+    match(r".{6}(\w+).{15}(\d+).{23}\w?\s(.*)", l), 
+readlines("input")))
