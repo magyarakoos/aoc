fe6c39501e32b0844f5c94d43bf593eaf508d045
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 12:14:32 +0200

    arch descktop sync: 2024-04-13 12:14:32

--- 2022/15/part1.jl
+++ 2022/15/part1.jl
@@ -1,8 +1,11 @@
 ∆(a, b) = abs(a[1] - b[1]) + abs(a[2] - b[2])
 const rx = r".{12}(-?\d+).{4}(-?\d+).{25}(-?\d+).{4}(-?\d+)"
 const y::Int = 10
+intvs = []; beacons = []
 for l in eachline(open("input", "r"))
     m = [parse.(Int, match(rx, l)[i]) for i in 1:4]
     ps = (m[1], m[2]); pb = (m[3], m[4])
+    if (pb[])
     d = ∆(ps, pb); h = ∆(ps, (ps[1], y))
+
 end
