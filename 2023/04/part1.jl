d3b925fec43fc32da2397af55d1752acc4861ecd
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 08:26:46 +0200

    arch descktop sync: 2024-04-13 08:26:46

--- 2023/04/part1.jl
+++ 2023/04/part1.jl
@@ -2,6 +2,6 @@ f = open("input", "r")
 rx = r"Card \d+: ((?:\d+\s+)+)\| ((?:\d+\s+)+)"
 (a = []; b = [])
 for l in eachline(f)
-    push!(a, rx.captures[1])
-    push!(b, rx.captures[2])
+    push!(a, match(rx, l)[1])
+    push!(b, match(rx, l)[2])
 end
