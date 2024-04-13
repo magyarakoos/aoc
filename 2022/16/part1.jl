26c7da71f6dd7deeb5dd74305be72a19fca8209d
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 17:08:34 +0200

    arch descktop sync: 2024-04-13 17:08:34

--- 2022/16/part1.jl
+++ 2022/16/part1.jl
@@ -1,10 +1,8 @@
-
-
 f = map(a -> [a[1], parse.(Int, a[2]), split(a[3], ", ")], 
-    map(l -> 
-        match(r".{6}(\w+).{15}(\d+).{23}\w?\s(.*)", l), 
-        readlines("input")
-    )
+map(l -> 
+match(r".{6}(\w+).{15}(\d+).{23}\w?\s(.*)", l), 
+readlines("input")
+)
 )
 g = Dict(); rates = Dict()
 for (key, rate, values) in f
