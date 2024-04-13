cd42769a5705a2e7a438812e754bcfa89e20e296
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 08:40:34 +0200

    arch descktop sync: 2024-04-13 08:40:34

--- 2023/04/part1.jl
+++ 2023/04/part1.jl
@@ -4,6 +4,7 @@ p1 = 0
 for l in eachline(f)
     a = parse.(Int, split(match(rx, l)[1]))
     b = parse.(Int, split(match(rx, l)[2]))
-    global p1 += 2 ^ (length(intersect(a, b)) - 1)
+    sz = length(intersect(a, b))
+    if (sz)
 end
 println(p1)
