90528e0beded1cab1443088c3cc569d2899aedf9
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 11:28:02 +0200

    arch descktop sync: 2024-04-13 11:28:02

--- 2022/13/part1.jl
+++ 2022/13/part1.jl
@@ -1,3 +1,7 @@
 f = map(s->split(s, "\n")[1:2], split(open(io->read(io, String), "input"),"\n\n"))
-for 
-println(f)
+for p in f
+    a = p[1]; b = p[2]
+    println(a)
+    println(b)
+    println()
+end
