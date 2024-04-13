f9843649acdce80a4968738a9fe4593c165548f5
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 11:50:26 +0200

    arch descktop sync: 2024-04-13 11:50:26

--- 2022/15/part1.jl
+++ 2022/15/part1.jl
@@ -1,5 +1,7 @@
 rx = r"/.{12}(-?\d+).{4}(-?\d+).{25}(-?\d+).{4}(-?\d+)/gm"
 open("input") do f
-    m = [parse.(Int, match(rx, readline(f))[i]) for i in 1:4]
-    println(m)
+    # m = [parse.(Int, [i]) for i in 1:4]
+    println(readline(f))
+    println(match(rx, readline(f)))
 end
+f = open("input")
