fa674388212c386b3e6fbf6f4bb0d48c9f433597
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 14:30:29 +0200

    arch descktop sync: 2024-04-13 14:30:29

--- 2022/16/main.jl
+++ 2022/16/main.jl
@@ -1,2 +1,10 @@
-s = "ASD"; t = "ASDSADSA"
-println(issubsequence(s, t))
+function issub(s, t)
+    i, j = 1, 1
+    while i <= length(s) && j <= length(t)
+        if s[i] == t[j]
+            i += 1
+        end
+        j += 1
+    end
+    return i > length(s)
+end
