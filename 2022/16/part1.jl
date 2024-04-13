fd70f3986aeb72430cc5432198b12b685a9fb791
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 13:48:32 +0200

    arch descktop sync: 2024-04-13 13:48:32

--- 2022/16/part1.jl
+++ 2022/16/part1.jl
@@ -1,16 +1,5 @@
-# f = map(
-#     map(a -> (a[1], parse.(a[2]), split(a[3:end], ", ")), 
-#         l -> match(r".{6}(\w+).{15}(\d+).{23}\w?\s(.*)",l)), 
-#     open("input", "r")
-# )
-
-f = map(l -> match(r".{6}(\w+).{15}(\d+).{23}\w?\s(.*)", l), 
-    readlines("input"))
-
-# f = readlines("input")
-# for l in f
-#     for s in match(r".{6}(\w+).{15}(\d+).{23}\w?\s(.*)", l)
-#         println(s)
-#     end
-#     println()
-# end
+f = map(
+    map(a -> (a[1], parse.(a[2]), split(a[3:end], ", ")), 
+        l -> match(r".{6}(\w+).{15}(\d+).{23}\w?\s(.*)",l)), 
+    readlines("input")
+)
