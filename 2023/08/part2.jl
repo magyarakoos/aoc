e834ea5521d2b457734fc6571bb3b22033538d9f
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 11:08:31 +0200

    arch descktop sync: 2024-04-13 11:08:31

--- 2023/08/part2.jl
+++ 2023/08/part2.jl
@@ -2,10 +2,14 @@ f = open("input", "r")
 inst = readline(f)
 readline(f)
 g = Dict()
+starts = []
 for l in eachline(f)
     g[l[1:3]] = (l[8:10], l[13:15])
+    if (l[3] == 'A')
+        push!(starts, l[1:3])
+    end
 end
-i = 
+p2 = 1
 for s in starts
     i = 0
     while (s[3] != 'Z')
