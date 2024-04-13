90808ed139244e8ac962cc6e9341c006f9bea2e9
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 08:34:14 +0200

    arch descktop sync: 2024-04-13 08:34:14

--- 2023/04/part1.jl
+++ 2023/04/part1.jl
@@ -3,4 +3,5 @@ rx = r"Card \d+: (\d+(?:\s+\d+)*) \| (\d+(?:\s+\d+)*)"
 for l in eachline(f)
     a = parse.(Int, split(match(rx, l)[1]))
     b = parse.(Int, split(match(rx, l)[2]))
+    println(intersect(a, b))
 end
