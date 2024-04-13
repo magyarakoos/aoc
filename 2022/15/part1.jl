b02ea9b826b45acb38a84fd0ccb64aaa8bfc2e92
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 12:32:55 +0200

    arch descktop sync: 2024-04-13 12:32:55

--- 2022/15/part1.jl
+++ 2022/15/part1.jl
@@ -1,23 +1,23 @@
-∆(a, b) = abs(a[1] - b[1]) + abs(a[2] - b[2])
-const rx = r".{12}(-?\d+).{4}(-?\d+).{25}(-?\d+).{4}(-?\d+)"
-const y::Int = 10
-intvs = Set(); beacons = Set(); beacons2 = Set();
-for l in eachline(open("input", "r"))
-    m = [parse.(Int, match(rx, l)[i]) for i in 1:4]
-    ps = (m[1], m[2]); pb = (m[3], m[4])
-    if pb[2] == y
-        push!(beacons, pb[1])
-    d = ∆(ps, pb) - ∆(ps, (ps[1], y))
-    push!(intvs, (ps[1] - d, ps[1] + d))
-end
+# ∆(a, b) = abs(a[1] - b[1]) + abs(a[2] - b[2])
+# const rx = r".{12}(-?\d+).{4}(-?\d+).{25}(-?\d+).{4}(-?\d+)"
+# const y::Int = 10
+# intvs = Set(); beacons = Set(); beacons2 = Set();
+# for l in eachline(open("input", "r"))
+#     m = [parse.(Int, match(rx, l)[i]) for i in 1:4]
+#     ps = (m[1], m[2]); pb = (m[3], m[4])
+#     if pb[2] == y
+#         push!(beacons, pb[1])
+#     d = ∆(ps, pb) - ∆(ps, (ps[1], y))
+#     push!(intvs, (ps[1] - d, ps[1] + d))
+# end
 
-for (b, e) in intvs, x in beacons
-    if b <= x && x <= e
-        push!(beacons2, x)
-    end
-end
+# for (b, e) in intvs, x in beacons
+#     if b <= x && x <= e
+#         push!(beacons2, x)
+#     end
+# end
 
-
+intv = [(1, 3), ()]
 
 intvs2 = []
 for intv in intvs
