b916c2441dca6138cb554fefba78de05527872e4
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 12:08:15 +0200

    arch descktop sync: 2024-04-13 12:08:15

--- 2022/15/part1.jl
+++ 2022/15/part1.jl
@@ -1,12 +1,11 @@
 ∆(a, b) = abs(a[1] - b[1]) + abs(a[2] - b[2])
 const rx = r".{12}(-?\d+).{4}(-?\d+).{25}(-?\d+).{4}(-?\d+)"
 const y::Int = 10
-s = Set()
-beacons = []
+s = Set(); beacons = Set();
 for l in eachline(open("input", "r"))
     m = [parse.(Int, match(rx, l)[i]) for i in 1:4]
     ps = (m[1], m[2]); pb = (m[3], m[4])
-    push!(beacons)
+    push!(beacons, pb)
     if (∆(ps, (ps[1], y)) > ∆(ps, pb))
         continue
     end
@@ -17,4 +16,4 @@ for l in eachline(open("input", "r"))
         end
     end
 end
-println(length(s))
+println(length(setdiff(s, beacons)))
