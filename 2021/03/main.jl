fc38716d59c9b1d10be3fa2278e85064b645c971
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 23:37:46 +0200

    arch descktop sync: 2024-04-13 23:37:46

--- 2021/03/main.jl
+++ 2021/03/main.jl
@@ -1,8 +1,8 @@
 a = [parse.(Int, split(l, "")) for l in eachline("input")]
-zc = zeros(length(a)); oc = zeros(length(a))
+zc = zeros(length(a[1])); oc = zeros(length(a[1]))
 for j in eachindex(a[1]), i in eachindex(a)
     if (a[i][j] == 1) global oc[j] += 1
-    else gloal zc[j] += 1 end
+    else global zc[j] += 1 end
 end
 println(zc)
 println(oc)
