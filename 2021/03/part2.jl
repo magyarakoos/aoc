e940ca57c8642a7e35064de586d0e346be5cf6a2
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 23:56:11 +0200

    arch descktop sync: 2024-04-13 23:56:11

--- 2021/03/part2.jl
+++ 2021/03/part2.jl
@@ -1,19 +1,22 @@
-a = [parse.(Int, split(l, "")) for l in eachline("input")]
-zc = zeros(length(a[1])); oc = zeros(length(a[1]))
-for j in eachindex(a[1]), i in eachindex(a)
-    if (a[i][j] == 1) global oc[j] += 1
-    else global zc[j] += 1 end
-end
-for j in eachindex(zc)
-    zc[j] = zc[j] <= oc[j] ? 0 : 1
-end
-reverse!(zc)
-g = 0; e = 0
-for j in eachindex(zc)
-    if zc[j]
-        global g |= (1 << (j - 1))
-    else
-        global e |= (1 << (j - 1))
+function f(a)
+    for j in eachindex(a[1]), i in eachindex(a)
+        if (a[i][j] == 1) global oc[j] += 1
+        else global zc[j] += 1 end
+    end
+    for j in eachindex(zc)
+        zc[j] = zc[j] <= oc[j] ? 0 : 1
+    end
+    reverse!(zc)
+    b = []
+    for j in eachindex(zc)
+        if zc[j]
+            global g |= (1 << (j - 1))
+        else
+            global e |= (1 << (j - 1))
+        end
     end
 end
+
+a = [parse.(Int, split(l, "")) for l in eachline("input")]
+zc = zeros(length(a[1])); oc = zeros(length(a[1]))
 println(g * e)
