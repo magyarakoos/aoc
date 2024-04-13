02467fc23131a0f85293b9a797f2014fe516538e
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 09:47:46 +0200

    arch descktop sync: 2024-04-13 09:47:46

--- 2023/08/part1.jl
+++ 2023/08/part1.jl
@@ -12,7 +12,7 @@ end
 f = open("input", "r")
 inst = readline(f)
 readline(f)
-g = Dict()
+g = Dict{String, String}()
 for l in eachline(f)
     a, b, c = [match(r"(\w+) = \((\w+), (\w+)\)", l)[i] for i in 1:3]
     g[a] = [b, c]
