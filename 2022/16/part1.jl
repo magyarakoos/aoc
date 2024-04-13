4176ae5220dd10613840c063fd28082025f27dc0
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 13:57:54 +0200

    arch descktop sync: 2024-04-13 13:57:54

--- 2022/16/part1.jl
+++ 2022/16/part1.jl
@@ -7,9 +7,7 @@ f = map(a -> [a[1], parse.(Int, a[2]), split(a[3], ", ")],
 g = Dict(); rates = Dict()
 for (key, rate, values) in f
     rates[key] = rate
-    
-    println(key)
-    println(rate)
-    println(values)
-    println()
+    for value in values
+        push!(g[key], value)
+    end
 end
