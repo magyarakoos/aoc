3a60ea7f34ed97ea0267a366f5da0ca8eb24f862
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 23:53:19 +0200

    arch descktop sync: 2024-04-13 23:53:19

--- 2021/03/main.jl
+++ 2021/03/main.jl
@@ -5,7 +5,9 @@ for j in eachindex(a[1]), i in eachindex(a)
     if (a[i][j] == 1) global oc[j] += 1
     else global zc[j] += 1 end
 end
-
+for j in eachindex(zc)
+    zc[j] = zc[j] < oc[j] ? 0 : 1
+end
 reverse!(zc)
 reverse!(oc)
 g = 0; e = 0
