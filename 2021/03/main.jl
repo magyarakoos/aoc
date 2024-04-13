9b861055732d90e3e15f416be274cb06f5b714e7
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 23:52:44 +0200

    arch descktop sync: 2024-04-13 23:52:44

--- 2021/03/main.jl
+++ 2021/03/main.jl
@@ -1,4 +1,3 @@
-function f()
 
 a = [parse.(Int, split(l, "")) for l in eachline("input")]
 zc = zeros(length(a[1])); oc = zeros(length(a[1]))
@@ -6,6 +5,7 @@ for j in eachindex(a[1]), i in eachindex(a)
     if (a[i][j] == 1) global oc[j] += 1
     else global zc[j] += 1 end
 end
+
 reverse!(zc)
 reverse!(oc)
 g = 0; e = 0
