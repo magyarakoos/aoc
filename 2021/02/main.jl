fc05162127556c59101e051fd43bcb32f2f797fc
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 23:10:08 +0200

    arch descktop sync: 2024-04-13 23:10:08

--- 2021/02/main.jl
+++ 2021/02/main.jl
@@ -1,4 +1,6 @@
 f = map(l -> (split(l)[1][1], parse(Int, split(l)[2])), readlines("input"))
-println(sum(map(x -> x[2], filter(t -> t[1] == 'f', f))) * 
-
+println(
+    sum(map(x -> x[2], filter(t -> t[1] == 'f', f))) * 
+    map(x -> x, filter(t -> t[1] != 'f', f))
+)
 # input.Where(t => t.task != 'f').Select(t => t.task == 'd' ? t.x : -t.x).Sum());
