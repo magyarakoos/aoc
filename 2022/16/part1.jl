0989641625de50dc0613243cae7c29fb13e7ca82
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 17:26:33 +0200

    arch descktop sync: 2024-04-13 17:26:33

--- 2022/16/part1.jl
+++ 2022/16/part1.jl
@@ -6,25 +6,28 @@ f = map(a -> [a[1], parse(Int, a[2]), split(a[3], ", ")],
     )
 )
 g = Dict(); rates = Dict(); dists = Dict()
-key_list = Vector{String}()
+key_list = Vector{String}()  # Renamed from 'keys' to avoid conflicts
 for (key, rate, values) in f
-    i = findfirst(x -> x == key, keys)
+    key_str = string(key)  # Convert key to a string
+    i = findfirst(key_list, key_str)
     if isnothing(i)
-        push!(keys, key)
-        i = length(keys)
+        push!(key_list, key_str)
+        i = length(key_list)
     end
     rates[i] = rate
     g[i] = Vector{String}()
     for value in values
-        j = findfirst(x -> x == value, keys)
+        value_str = string(value)  # Convert value to a string
+        j = findfirst(key_list, value_str)
         if isnothing(j)
-            push!(keys, value)
-            j = length(keys)
+            push!(key_list, value_str)
+            j = length(key_list)
         end
         push!(g[i], j)
     end
     global i += 1
 end
+
 for i in eachindex(g)
     println(g[i])
 end
