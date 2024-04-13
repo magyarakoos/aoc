eac3c5711f69ee942895cb77c1a658eafbd230d0
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 12:00:46 +0200

    arch descktop sync: 2024-04-13 12:00:46

--- 2022/15/part1.jl
+++ 2022/15/part1.jl
@@ -2,7 +2,7 @@
 ∆(a, b) = abs(a[1] - b[1]) + abs(a[2] - b[2])
 const rx = r".{12}(-?\d+).{4}(-?\d+).{25}(-?\d+).{4}(-?\d+)"
 const y::Int = 2e5
-
+s = Set()
 for l in eachline(open("input", "r"))
     m = [parse.(Int, match(rx, l)[i]) for i in 1:4]
     ps = (m[1], m[2]); pb = (m[3], m[4])
