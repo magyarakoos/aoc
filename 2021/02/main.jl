d01bc63fbe2b9f384f8e9adce20f7cbd4da40228
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 23:11:53 +0200

    arch descktop sync: 2024-04-13 23:11:53

--- 2021/02/main.jl
+++ 2021/02/main.jl
@@ -4,13 +4,15 @@ println(
     sum(map(x -> x[1] == 'd' ? x[2] : -x[2], filter(t -> t[1] != 'f', f)))
 )
 hpos = 0; depth = 0; aim = 0
-
-for (int i = 0; i < input.Length; i++) {
-    if (input[i].task == 'd') aim += input[i].x;
-    else if (input[i].task == 'u') aim -= input[i].x;
-    else {
-        hpos += input[i].x;
-        depth += aim * input[i].x;
-    }
-}
-Console.WriteLine(hpos * depth);
+for (t, x) in f
+    if t == 'd' 
+end
+# for (int i = 0; i < input.Length; i++) {
+#     if (input[i].task == 'd') aim += input[i].x;
+#     else if (input[i].task == 'u') aim -= input[i].x;
+#     else {
+#         hpos += input[i].x;
+#         depth += aim * input[i].x;
+#     }
+# }
+# Console.WriteLine(hpos * depth);
