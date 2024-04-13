a44b3bea04cee0214514a088f0cfe9933171fbe8
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 19:09:02 +0200

    arch descktop sync: 2024-04-13 19:09:02

--- 2022/18/main.jl
+++ 2022/18/main.jl
@@ -1,3 +1,6 @@
 s = Set()
 f = (m) -> push!(s, (m[1], m[2], m[3]))
 map(l -> f(map(x -> parse(Int, x), match(r"(\d+),(\d+),(\d+)", l))), readlines("input"))
+res = length(s) * 6
+for p in res
+    if ()
