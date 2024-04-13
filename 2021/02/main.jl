dbfe1bdf640ea22889a0433cc1f551d46605c95a
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 23:06:06 +0200

    arch descktop sync: 2024-04-13 23:06:06

--- 2021/02/main.jl
+++ 2021/02/main.jl
@@ -1,3 +1,3 @@
-pos = 0, depth = 0
+pos = 0; depth = 0
 f = (t, x) -> (t == 'f' ? pos += x : t == 'd' ? depth += x : depth -= x)
-map(l -> (split(l)[1][1], parse(Int, split(l)[2])), readlines("input"))
+map(l -> f(split(l)[1][1], parse(Int, split(l)[2])), readlines("input"))
