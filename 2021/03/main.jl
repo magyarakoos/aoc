26de4322c94a5bc55e9ec29796d28080208b35a6
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 23:20:32 +0200

    arch descktop sync: 2024-04-13 23:20:32

--- 2021/03/main.jl
+++ 2021/03/main.jl
@@ -1,3 +1,11 @@
-a = map(x -> [x[i] for i in eachindex(x)], readlines("input"))
-a_tr = a'
+# Read lines from the file
+lines = readlines("input")
+
+# Split each line into individual characters and collect them into a 2D array
+a = [collect(line) for line in lines]
+
+# Transpose the array of characters
+a_tr = transpose(a)
+
+# Print the transposed array
 println(a_tr)
