72cb88314856e0ecd97b73fa68920761d788c682
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 23:11:18 +0200

    arch descktop sync: 2024-04-13 23:11:18

--- 2021/02/main.jl
+++ 2021/02/main.jl
@@ -3,4 +3,14 @@ println(
     sum(map(x -> x[2], filter(t -> t[1] == 'f', f))) * 
     sum(map(x -> x[1] == 'd' ? x[2] : -x[2], filter(t -> t[1] != 'f', f)))
 )
-# input.Where(t => t.task != 'f').Select(t => t.task == 'd' ? t.x : -t.x).Sum());
+hpos = 0; depth = 0; aim = 0
+
+for (int i = 0; i < input.Length; i++) {
+    if (input[i].task == 'd') aim += input[i].x;
+    else if (input[i].task == 'u') aim -= input[i].x;
+    else {
+        hpos += input[i].x;
+        depth += aim * input[i].x;
+    }
+}
+Console.WriteLine(hpos * depth);
