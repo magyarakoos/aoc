a6e713265fb76573c9e895dac981d0d7a7d17ad2
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 09:35:59 +0200

    arch descktop sync: 2024-04-13 09:35:59

--- 2023/08/part1.jl
+++ 2023/08/part1.jl
@@ -6,7 +6,7 @@ for l in eachline(f)
     g[a] = (b, c)
 end
 wander = (curr, i) -> begin
-    return curr == "ZZZ" ? 0 :
-    1 + wander(g[curr][inst[i % length(inst)] == 'L' ? 1 : 0], i + 1)
+return curr == "ZZZ" ? 0 :
+1 + wander(g[curr][inst[i % length(inst)] == 'L' ? 1 : 0], i + 1)
 end
 println(wander("AAA"))
