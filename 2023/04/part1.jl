022190c2af41f05436b7f4fbedf76876ff331206
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 09:04:21 +0200

    arch descktop sync: 2024-04-13 09:04:21

--- 2023/04/part1.jl
+++ 2023/04/part1.jl
@@ -10,12 +10,10 @@ for l in eachline(f)
     end
     push!(p2, (sz, 1))
 end
-for i in 1:length()
-    for j in (i + 1):(i + m)
-        if j > length(p2)
-            break
-        end
-        p2[j][2] += c
+for i in 1:length(p2), j in (i + 1):(i + m)
+    if j > length(p2)
+        break
     end
+    p2[j][2] += c
 end
 println((p1, sum(map(x -> x[2], p2))))
