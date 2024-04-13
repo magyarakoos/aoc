7a625c1c403d1b72b23ffdcf66512be9bc71368d
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 11:56:46 +0200

    arch descktop sync: 2024-04-13 11:56:46

--- 2022/15/part1.jl
+++ 2022/15/part1.jl
@@ -1,10 +1,10 @@
-∆(p1, p2) = abs(p1[])
+∆(a, b) = abs(a[1] - b[1]) + abs(a[2] - b[2])
 rx = r".{12}(-?\d+).{4}(-?\d+).{25}(-?\d+).{4}(-?\d+)"
 
 for l in eachline(open("input", "r"))
     m = [parse.(Int, match(rx, l)[i]) for i in 1:4]
     ps = (m[1], m[2]); pb = (m[3], m[4])
     println((ps, pb))
-
+    ∆(ps, pb)
 
 end
