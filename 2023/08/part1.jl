239a6f41d4acbf1a076b327d01ee0ad430c44e1c
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 11:00:27 +0200

    arch descktop sync: 2024-04-13 11:00:27

--- 2023/08/part1.jl
+++ 2023/08/part1.jl
@@ -5,9 +5,12 @@ g = Dict()
 starts = []
 for l in eachline(f)
     g[l[1:3]] = (l[8:10], l[13:15])
-    if (l[3] == 'A') {
-        
-    }
+    if (l[3] == 'A')
+        push!(starts, l[1:3])
+    end
+end
+for s in starts
+    
 end
 (p1 = 0; p2 = 0)
 (i = 1; s = "AAA")
