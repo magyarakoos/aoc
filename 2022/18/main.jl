a02850c6fc3a8c6fbb31ff22dcae5a0673ad6d29
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 19:07:19 +0200

    arch descktop sync: 2024-04-13 19:07:19

--- 2022/18/main.jl
+++ 2022/18/main.jl
@@ -1,3 +1,3 @@
 s = Set()
-f = (m) -> push!(s, (parse(Int, m[1]), parse(Int,)))
-map(l -> match(r"(\d+),(\d+),(\d+)", l), readlines("input"))
+f = (m) -> push!(s, (parse(Int, m[1]), parse(Int, m[2])))
+map(l -> map(x -> parse(Int, x), match(r"(\d+),(\d+),(\d+)", l)), readlines("input"))
