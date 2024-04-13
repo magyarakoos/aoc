e284dae0f4f0e34a42986b53d6e5442227b8d16e
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 15:36:34 +0200

    arch descktop sync: 2024-04-13 15:36:34

--- 2022/16/main.jl
+++ 2022/16/main.jl
@@ -3,11 +3,11 @@ t = lowercase(readline())
 if (t[3] == 'x')
     t = t[1:2]
 end
-i, j = 1, 1
+i = 1; j = 1
 while i <= length(s) && j <= length(t)
     if s[i] == t[j]
-        j += 1
+        global j += 1
     end
-    i += 1
+    global i += 1
 end
 println(i > length(s) ? "Yes" : "No")
