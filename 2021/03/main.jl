5adcfe5dc21154ec1f1a388eec3781c537ad11e2
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 23:23:59 +0200

    arch descktop sync: 2024-04-13 23:23:59

--- 2021/03/main.jl
+++ 2021/03/main.jl
@@ -1,13 +1,3 @@
-# Open the file and read lines
-file = open("input", "r")
-lines = readlines(file)
-close(file)
-
-# Create a 2D vector of characters
-a::Vector{Vector} = [collect(line) for line in lines]
-
-# Transpose the 2D vector
-a_tr = transpose(a)
-
-# Print the transposed 2D vector
-println(a_tr)
+a = map(x -> [parse(Int, x[i]) for i in eachindex(x)], readlines("input"))
+a = a'
+p
