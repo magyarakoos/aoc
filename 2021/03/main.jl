73010b0cc3934276cdf50bb05613931dc8611acc
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 23:35:27 +0200

    arch descktop sync: 2024-04-13 23:35:27

--- 2021/03/main.jl
+++ 2021/03/main.jl
@@ -1,5 +1,6 @@
 a = [parse.(Int, split(l, "")) for l in eachline("input")]
-zc = Vector{Int}(length(a))
+zc = Vector{Int}(length(a)); oc = zc
 for j in eachindex(a[1]), i in eachindex(a)
-    res[j][]
+    if (a[i][j] == 1)
+        
 end
