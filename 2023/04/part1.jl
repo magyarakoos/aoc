e59c8e6f1ba3956f9bdef1f693c6efecc398025d
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 08:30:47 +0200

    arch descktop sync: 2024-04-13 08:30:47

--- 2023/04/part1.jl
+++ 2023/04/part1.jl
@@ -2,7 +2,7 @@ f = open("input", "r")
 rx = r"Card \d+: (\d+(?:\s+\d+)*) \| (\d+(?:\s+\d+)*)"
 (a = []; b = [])
 for l in eachline(f)
-    push!(a, match(rx, l)[1])
+    push!(a, parse.(Int, match(rx, l)[1]))
     push!(b, match(rx, l)[2])
 end
 println(a)
