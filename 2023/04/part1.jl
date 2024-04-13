471bce104031aac1bb717fa3ba066896b2a94a17
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 09:00:48 +0200

    arch descktop sync: 2024-04-13 09:00:48

--- 2023/04/part1.jl
+++ 2023/04/part1.jl
@@ -1,7 +1,6 @@
 f = open("input", "r")
 rx = r"Card\s+\d+:((?:\s+\d+)+) \|((?:\s+\d+)+)"
-p1 = 0
-m = []
+(p1 = 0; m = [])
 for l in eachline(f)
     a = parse.(Int, split(match(rx, l)[1]))
     b = parse.(Int, split(match(rx, l)[2]))
