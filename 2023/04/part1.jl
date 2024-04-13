762019345945b9f67f5602ecaf29d8fec10356d5
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 08:43:27 +0200

    arch descktop sync: 2024-04-13 08:43:27

--- 2023/04/part1.jl
+++ 2023/04/part1.jl
@@ -6,6 +6,7 @@ for l in eachline(f)
     b = parse.(Int, split(match(rx, l)[2]))
     sz = length(intersect(a, b))
     # global p1 += sz != 0 ? 2 ^ (sz - 1) : 0
-    global p1 += sz * (2.0 ^ (sz - 1))
+    if ()
+    global p1 += Bool(sz) * (2.0 ^ (sz - 1))
 end
-println(Int{p1})
+println(Int(p1))
