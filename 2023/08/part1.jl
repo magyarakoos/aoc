be44864176077dfba3ca7973b4bf9ffc1be6db43
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 09:39:25 +0200

    arch descktop sync: 2024-04-13 09:39:25

--- 2023/08/part1.jl
+++ 2023/08/part1.jl
@@ -6,7 +6,6 @@ for l in eachline(f)
     a, b, c = [match(r"(\w+) = \((\w+), (\w+)\)", l)[i] for i in 1:3]
     g[a] = (b, c)
 end
-exit(0)
 wander = (curr, i) -> begin
 return curr == "ZZZ" ? 0 :
 1 + wander(g[curr][inst[i % length(inst)] == 'L' ? 1 : 0], i + 1)
