04f61f5ae4140d5a2948d1c4dc5410787932c947
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 19:17:03 +0200

    arch descktop sync: 2024-04-13 19:17:03

--- 2022/18/main.jl
+++ 2022/18/main.jl
@@ -2,7 +2,7 @@
 λ = (m) -> push!(σ, (m[1], m[2], m[3]))
 Ψ = (x, y, z) -> [(x+1, y, z), (x-1, y, z), (x, y+1, z), (x, y-1, z), (x, y, z+1), (x, y, z-1)]
 map(l -> λ(map(x -> parse(Int, x), match(r"(\d+),(\d+),(\d+)", l))), readlines("input"))
-Σ = length(s) * 6
+Σ = length(σ) * 6
 for (x, y, z) in σ, ρ in Ψ(x, y, z)
     if in(σ, ρ)
         global Σ -= 1
