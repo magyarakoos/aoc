4c7e0be115ab4723caf4edd43e710e2f0a7c0c2b
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 08:46:59 +0200

    arch descktop sync: 2024-04-13 08:46:59

--- 2023/04/part1.jl
+++ 2023/04/part1.jl
@@ -2,6 +2,7 @@ f = open("input", "r")
 rx = r"Card\s+\d+:((?:\s+\d+)+) \|((?:\s+\d+)+)"
 p1 = 0
 matches = []
+
 for l in eachline(f)
     a = parse.(Int, split(match(rx, l)[1]))
     b = parse.(Int, split(match(rx, l)[2]))
@@ -9,5 +10,9 @@ for l in eachline(f)
     if (sz != 0)
         global p1 += 2 ^ (sz - 1)
     end
+    push!(matches, sz)
 end
-println(Int(p1))
+
+
+
+println(p1)
