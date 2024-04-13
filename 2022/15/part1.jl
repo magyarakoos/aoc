c537c101f9d78e953c2bc9691017acb3cf83f94a
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 11:54:28 +0200

    arch descktop sync: 2024-04-13 11:54:28

--- 2022/15/part1.jl
+++ 2022/15/part1.jl
@@ -2,5 +2,6 @@ rx = r".{12}(-?\d+).{4}(-?\d+).{25}(-?\d+).{4}(-?\d+)"
 
 for l in eachline(open("input", "r"))
     m = [parse.(Int, match(rx, l)[i]) for i in 1:4]
-    p1 = 
+    ps = (m[1], m[2]); pb = (m[3], m[4])
+    println((ps, pb))
 end
