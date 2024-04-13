b976e0056a2bff4e7bcf7b3bd7422cab8beb87c5
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 11:10:48 +0200

    arch descktop sync: 2024-04-13 11:10:48

--- 2023/08/part1.jl
+++ 2023/08/part1.jl
@@ -7,7 +7,7 @@ for l in eachline(f)
 end
 i = 0; s = "AAA"
 while (s != "ZZZ")
-    s = g[s][inst[(i % length(inst)) + 1] == 'L' ? 1 : 2]
-    i += 1
+    global s = g[s][inst[(i % length(inst)) + 1] == 'L' ? 1 : 2]
+    global i += 1
 end
 println(i)
