6540c476823bd68e400d8ce1cf773c1ec632db40
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 09:41:43 +0200

    arch descktop sync: 2024-04-13 09:41:43

--- 2023/08/part1.jl
+++ 2023/08/part1.jl
@@ -2,8 +2,7 @@ function wander(curr, i, inst, g)
     return curr == "ZZZ" ? 0 :
     wander(
         g[curr][inst[i % length(inst)] == 'L' ? 1 : 0], 
-        i + 1, inst,
-        g)
+        i + 1, inst, g)
 end
 
 f = open("input", "r")
@@ -14,8 +13,5 @@ for l in eachline(f)
     a, b, c = [match(r"(\w+) = \((\w+), (\w+)\)", l)[i] for i in 1:3]
     g[a] = (b, c)
 end
-wander = (curr, i) -> begin
-return curr == "ZZZ" ? 0 :
-1 + 
-end
-println(wander("AAA"))
+
+println(wander("AAA", 0, ))
