0649482d554158899a5ab6a57ab13538ecc3cd47
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-13 19:24:30 +0200

    arch descktop sync: 2024-04-13 19:24:30

--- 2022/18/main.jl
+++ 2022/18/main.jl
@@ -1,4 +1,4 @@
-σ = Set()
+σ = Set()Y
 λ = (m) -> push!(σ, (m[1], m[2], m[3]))
 Ψ = (x, y, z) -> [(x+1, y, z), (x-1, y, z), (x, y+1, z), (x, y-1, z), (x, y, z+1), (x, y, z-1)]
 map(l -> λ(map(x -> parse(Int, x), match(r"(\d+),(\d+),(\d+)", l))), readlines("input"))
