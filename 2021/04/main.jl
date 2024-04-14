bce162b760a128d43cc37bd0a56926916e1b3be3
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 11:03:59 +0200

    arch descktop sync: 2024-04-14 11:03:59

--- 2021/04/main.jl
+++ 2021/04/main.jl
@@ -1,6 +1,6 @@
-f = open("input", "r")
-n::Vector{Int} = map(x -> parse(Int, x), split(readline(f), ","))
-b = []
+f = open("input", "r"); b = []
+n = map(x -> parse(Int, x), split(readline(f), ","))
+
 while !eof(f)
     readline(f)
     board = []
