696f3519e6eb6315f1cc0d8c7d455cbd9c37dfb3
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 18:08:14 +0200

    arch descktop sync: 2024-04-14 18:08:14

--- 2021/09/part1.jl
+++ 2021/09/part1.jl
@@ -12,11 +12,11 @@ basins = []; sizes = []
     return true
 end
 φ = (i, j, k) -> begin
+    sizes[k] += 1  
     for k in 1:4
         ni = i + di[k]; nj = j + dj[k]
         if ni > 0 && nj > 0 && ni <= length(f) && nj <= length(f[1]) && !vis[ni, nj] && f[ni][nj] < 9
             vis[ni, nj] = true
-            sizes[k] += 1
             println((ni, nj, k))
             φ(ni, nj, k)
         end
@@ -29,4 +29,5 @@ for i in eachindex(basins)
     φ(basins[i][1], basins[i][2], i)
 end
 sort(sizes, rev=true)
+println(sizes)
 println("$(sum(x -> f[x[1]][x[2]], basins) + length(basins))\n$(prod(sizes[1:3]))")
