6c114035377ba173e0c2fbcc76b07dc525df3d58
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 10:59:02 +0200

    arch descktop sync: 2024-04-14 10:59:02

--- 2021/04/main.jl
+++ 2021/04/main.jl
@@ -20,7 +20,7 @@ for x in n
         end
         if bc[i, j] == 5 || bc[i, k + 5] == 5
             println(scores[i] * x)
-            exit(0)
+            global bc = zeros(Int, length(b), 10)
         end
     end
 end
