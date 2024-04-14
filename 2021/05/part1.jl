eaf20323e8d49eea53e63a75849f03cd4bb37e8b
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 11:29:17 +0200

    arch descktop sync: 2024-04-14 11:29:17

--- 2021/05/part1.jl
+++ 2021/05/part1.jl
@@ -1,8 +1,9 @@
 m = zeros(Int, 1000, 1000)
 λ = (p) -> for x in p[1]:p[3], y in p[2]:p[4] m[x, y] += 1 end
 map(p -> λ(p), filter(p -> p[1] == p[3] || p[2] == p[4],
-    map(l -> map(x -> parse(Int, x), 
+    map(l -> map(x -> parse(Int, x) + 1, 
         match(r"(\d+),(\d+) -> (\d+),(\d+)", l)), 
         readlines("input")
     ))
 )
+println()
