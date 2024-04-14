465ee0956ffdf94ec7ed11fe22412ff14f6d0589
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 11:45:16 +0200

    arch descktop sync: 2024-04-14 11:45:16

--- 2021/05/part1.jl
+++ 2021/05/part1.jl
@@ -1,10 +1,11 @@
 m = zeros(Int, 1000, 1000); m2 = m
-Ψ = (p) -> for 
+Ψ = (p) -> if p[1] == p[3] || p[2] == p[4] for 
     x in min(p[1], p[3]):max(p[1], p[3]), 
-    y in min(p[2], p[4]):max(p[2], p[4]) m[y, x] += 1 end
-ψ
+    y in min(p[2], p[4]):max(p[2], p[4]) 
+    m[y, x] += 1 end end
+ψ = (p) -> 
 map(, 
-    filter(p -> p[1] == p[3] || p[2] == p[4],
+    filter(p -> ,
     map(l -> map(x -> parse(Int, x) + 1, 
         match(r"(\d+),(\d+) -> (\d+),(\d+)", l)), 
         readlines("input")
