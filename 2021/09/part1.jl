000c63e9633d97970ed2ba253b30871261a3da72
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 15:12:45 +0200

    arch descktop sync: 2024-04-14 15:12:45

--- 2021/09/part1.jl
+++ 2021/09/part1.jl
@@ -1,4 +1,6 @@
 f = map(l -> split(l, ""), readlines("input"))
 m = zeros(Int, length(f), length(f[1]))
 for i in eachindex(f), j in eachindex(f[1])
-    
+    m[i, j] = parse(Int, f[i][j])
+end
+println(m)
