fbc4d06c00869ab9f824d35a4b037f2d73ada16a
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 13:51:13 +0200

    arch descktop sync: 2024-04-14 13:51:13

--- 2021/08/part2.jl
+++ 2021/08/part2.jl
@@ -1,4 +1,4 @@
 using Combinatorics
-f = map(l -> [match(r"(.*)\|(.*)", l)[i] for i in 1:2], readlines("input"))
+f = map(l -> [split(match(r"(.*)\|(.*)", l)[i]) for i in 1:2], readlines("input"))
 
 println(f)
