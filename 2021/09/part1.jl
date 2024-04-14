de41b3baf43ef1a180f27a57407cf3c5f5c42f51
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 15:15:53 +0200

    arch descktop sync: 2024-04-14 15:15:53

--- 2021/09/part1.jl
+++ 2021/09/part1.jl
@@ -4,5 +4,6 @@ for i in eachindex(f), j in eachindex(f[1])
     m[i, j] = parse(Int, f[i][j])
 end
 for i in eachindex(f), j in eachline(f[1]), di in [1, -1, 0, 0], dj in [0, 0, 1, -1]
-    if ()
+    if (0 < di && 0 < dj && di <= length(f) && dj <= length(f[1]))
+        
 end
