fb53a08ec83304076855208813aa77ca87b7b424
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 11:00:46 +0200

    arch descktop sync: 2024-04-14 11:00:46

--- 2021/04/main.jl
+++ 2021/04/main.jl
@@ -11,15 +11,17 @@ while !eof(f)
 end
 bc = zeros(Int, length(b), 10)
 scores = map(x -> sum(sum(x)), b)
-won =
+won = zeros(Bool, length(b))
 for x in n
     for i in eachindex(b), j in 1:5, k in 1:5
+        if won[i] continue end
         if b[i][j][k] == x
             scores[i] -= x
             bc[i, j] += 1
             bc[i, k + 5] += 1
         end
         if bc[i, j] == 5 || bc[i, k + 5] == 5
+            won[i] = 1
             println(scores[i] * x)
         end
     end
