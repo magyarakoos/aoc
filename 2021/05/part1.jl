5d46d3837b53aedfc06dc68d841236a1007737de
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 11:17:16 +0200

    arch descktop sync: 2024-04-14 11:17:16

--- 2021/05/part1.jl
+++ 2021/05/part1.jl
@@ -3,7 +3,7 @@ m = zeros(Int, 1000, 1000)
 # map((x1, y1, x2, y2) -> λ(x1, y1, x2, y2), 
    #where((x1, y1, x2, y2) -> x1 == x2 || y1 == y2, 
     map(l -> 
-        map(x -> (x[1], x[2], x[3], x[4]), 
-        match(r"(\d+),(\d+) -> (\d+),(\d+)", l)), 
-    readlines("input")
+        # map(x -> (x[1], x[2], x[3], x[4]), 
+        match(r"(\d+),(\d+) -> (\d+),(\d+)", l), 
+    readlines("input"))
 # )# )# )
