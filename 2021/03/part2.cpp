5f3c334edaaae0efe47c3015227899c0b817680e
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 07:31:55 +0200

    arch descktop sync: 2024-04-14 07:31:55

--- 2021/03/part2.cpp
+++ 2021/03/part2.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 int main() {
     vector<vector<bool>> v({});
+    vector<int> oc;
     ifstream f("input");
     string s;
     while (f >> s) {
-        for (int i = 0; i < ) {
-            v.back().push_back(c == '1');
+        for (int i = 0; i < s.size(); i++) {
+            v.back().push_back(s[i] == '1');
         }
         v.push_back({});
     }
