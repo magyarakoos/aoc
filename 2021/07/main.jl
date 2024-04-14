8e309f51454c756301a18525240df500bbb4caa1
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 13:13:34 +0200

    arch descktop sync: 2024-04-14 13:13:34

--- 2021/07/main.jl
+++ 2021/07/main.jl
@@ -4,7 +4,7 @@ for d in minimum(a):maximum(a)
     global p1 = min(p1, sum(x -> abs(x - d), a))
     global p2 = min(p2, sum(x -> abs(x - d), a))
     for x in a
-        println(collect(1:abs(a - d)))
+        println(1:abs(x - d))
     end
 end
 println("$p1\n$p2")
