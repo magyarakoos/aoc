814eaedb7567a2771c43b63c90ab438434a40e7d
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 15:40:23 +0200

    arch descktop sync: 2024-04-14 15:40:23

--- 2021/09/part1.jl
+++ 2021/09/part1.jl
@@ -3,10 +3,7 @@ f = map(l -> map(x -> parse(Int, x), split(l, "")), readlines("input"))
     di = [1, -1, 0, 0]; dj = [0, 0, 1, -1]
     for k in 1:4
         ni = i + di[k]; nj = j + dj[k]
-        if ni < 1 || nj < 1 || ni > length(f) || nj > length(f[1])
-            continue 
-        end
-        if f[ni][nj] <= f[i][j]
+        if ni > 0 &&  nj > 0 && ni <= length(f) && nj <= length(f[1]) && f[ni][nj] <= f[i][j]
             return false
         end
     end
