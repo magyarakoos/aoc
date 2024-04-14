772eb7baf2c6f15d19d403ab61e408976966472b
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 13:11:50 +0200

    arch descktop sync: 2024-04-14 13:11:50

--- 2021/07/main.jl
+++ 2021/07/main.jl
@@ -2,6 +2,6 @@ a = map(x -> parse(Int, x), split(readline("input"), ","))
 p1 = typemax(Int); p2 = typemax(Int)
 for d in minimum(a):maximum(a)
     global p1 = min(p1, sum(x -> abs(x - d), a))
-    global p2 = min(p2, sum(x -> sum(collect(1:abs(a - d)))))
+    global p2 = min(p2, sum(x -> sum(collect(1:abs(a - d))), a))
 end
 println("$p1\n$p2")
