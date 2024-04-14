f2c5bc89539764de74fdee532edf3e32d5d3b819
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 14:07:34 +0200

    arch descktop sync: 2024-04-14 14:07:34

--- 2021/08/part2.jl
+++ 2021/08/part2.jl
@@ -3,6 +3,7 @@ using Combinatorics
 f = map(l -> [split(match(r"(.*)\|(.*)", l)[i]) for i in 1:2], readlines("input"))
 for (a, sol) in f
     for perm in permutations("abcdefg")
+        taken = zeros(Char, 10)
         
     end
 end
