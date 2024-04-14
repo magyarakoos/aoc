4ba2207ac48efcd210d93829b02a4952a5a7f2f3
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 11:25:06 +0200

    arch descktop sync: 2024-04-14 11:25:06

--- 2021/05/part1.jl
+++ 2021/05/part1.jl
@@ -1,11 +1,9 @@
 m = zeros(Int, 1000, 1000)
-filter(p -> x1 == x2 || y1 == y2, 
-map(x -> [x[1], x[2], x[3], x[4]],
+filter(p -> x1 == x2 || y1 == y2,
 map(l -> 
 map(x -> parse(Int, x), 
 match(r"(\d+),(\d+) -> (\d+),(\d+)", l)), 
 readlines("input")
 ))
-)
 # λ = (x1, y1, x2, y2) -> for x in x1:x2, y in y1:y2 m[x, y] += 1 end
 # map((x1, y1, x2, y2) -> λ(x1, y1, x2, y2), 
