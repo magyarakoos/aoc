c1e08a1654ab0313591bfb3b3eefc4961f618b17
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 15:27:56 +0200

    arch descktop sync: 2024-04-14 15:27:56

--- 2021/09/part1.jl
+++ 2021/09/part1.jl
@@ -1,23 +1,17 @@
-f = map(l -> map(x -> parse(Int, x), split(l, ""), readlines("input"))
-m = zeros(Int, length(f), length(f[1]))
-for i in eachindex(f), j in eachindex(f[1])
-    m[i, j] = parse(Int, f[i][j])
-end
+f = map(l -> map(x -> parse(Int, x), split(l, "")), readlines("input"))
 ψ = (i, j) -> begin
     for di in [1, -1, 0, 0], dj in [0, 0, 1, -1]
-        if (0 < di && 0 < dj && di <= length(m) && dj <= length(m[1]) && m[di][dj] <= m[i][j])
+        if (0 < di && 0 < dj && di <= length(f) && dj <= length(f[1]) && f[di][dj] <= f[i][j])
             return false
         end
     end
     return true
 end
 p1 = 0
-for i in eachindex(m), j in eachindex(m[1])
+for i in eachindex(f), j in eachindex(f[1])
     if ψ(i, j) global p1 += m[i][j] + 1 end
 end
-for row in m
-    for x in row
-    end
+for row in f
     println()
 end
 println(p1)
