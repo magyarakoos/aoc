bac6d2aca607a5d40f86a204a40b69f48a31aa61
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 12:58:46 +0200

    arch descktop sync: 2024-04-14 12:58:46

--- 2021/06/part1.jl
+++ 2021/06/part1.jl
@@ -1,11 +1,12 @@
 pop = zeros(Int, 9)
 map(x -> pop[parse(Int, x) + 1] += 1, split(readline("input"), ","))
-λ = (cyc)
-for i in 1:80
-    cur = zeros(Int, 9)
-    for j in 2:9 cur[j - 1] = pop[j] end
-    cur[9] += pop[1]
-    cur[7] += pop[1]
-    global pop = copy(cur)
+λ = (cyc) -> begin
+    for i in 1:cyc
+        cur = zeros(Int, 9)
+        for j in 2:9 cur[j - 1] = pop[j] end
+        cur[9] += pop[1]
+        cur[7] += pop[1]
+        global pop = copy(cur)
+    end
 end
 println(sum(pop))
