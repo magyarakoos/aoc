ac4384dfdc8ed85672e6deba711f95d69134574e
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 14:18:36 +0200

    arch descktop sync: 2024-04-14 14:18:36

--- 2021/08/part2.jl
+++ 2021/08/part2.jl
@@ -13,7 +13,7 @@ f = map(l -> [split(match(r"(.*)\|(.*)", l)[i]) for i in 1:2], readlines("input"
 #     end
 # end
 
-taken = zeros(Char, 10)
+taken = fill(Char, 10)
 for s in a
     mask = zeros(Int, 7)
     for i in indexin(s, perm)
