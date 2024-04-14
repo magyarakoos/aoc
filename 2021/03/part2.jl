4d57aa6af2c6546fccadc399e80b14837a994c9c
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 08:30:43 +0200

    arch descktop sync: 2024-04-14 08:30:43

--- 2021/03/part2.jl
+++ 2021/03/part2.jl
@@ -1,9 +1,10 @@
 f = open("input", "r")
-a = []
+a = []; w = 0
 for l in eachline(f)
-    x = 0
+    x = 0; w = length(l)
     for i in eachindex(l)
         x |= ((l[i] == '1') << (length(l) - i))
     end
     push!(a, x)
 end
+Ψ = ()
