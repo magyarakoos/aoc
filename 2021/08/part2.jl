1f08bdf45107d308de115c2e20283b7358681115
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 14:18:02 +0200

    arch descktop sync: 2024-04-14 14:18:02

--- 2021/08/part2.jl
+++ 2021/08/part2.jl
@@ -1,15 +1,23 @@
 using Combinatorics
 ⊗ = [[1,1,1,0,1,1,1],[0,0,1,0,0,1,0],[1,0,1,1,1,0,1],[1,0,1,1,0,1,1],[0,1,1,1,0,1,0],[1,1,0,1,0,1,1],[1,1,0,1,1,1,1],[1,0,1,0,0,1,0],[1,1,1,1,1,1,1],[1,1,1,1,0,1,1]] 
 f = map(l -> [split(match(r"(.*)\|(.*)", l)[i]) for i in 1:2], readlines("input"))
-for (a, sol) in f
-    for perm in permutations("abcdefg")
-        taken = zeros(Char, 10)
-        for s in a
-            mask = zeros(Int, 7)
-            for i in indexin(s, perm)
-                mask[i] = 1
-            end
-        end
-    end
-end
+# for (a, sol) in f
+#     for perm in permutations("abcdefg")
+#         taken = zeros(Char, 10)
+#         for s in a
+#             mask = zeros(Int, 7)
+#             for i in indexin(s, perm)
+#                 mask[i] = 1
+#             end
+#         end
+#     end
+# end
 
+taken = zeros(Char, 10)
+for s in a
+    mask = zeros(Int, 7)
+    for i in indexin(s, perm)
+        mask[i] = 1
+    end
+    println((s, mask))
+end
