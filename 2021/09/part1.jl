a0cc76f695d577d1b923d46692f3c94423b27bca
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 15:34:57 +0200

    arch descktop sync: 2024-04-14 15:34:57

--- 2021/09/part1.jl
+++ 2021/09/part1.jl
@@ -2,7 +2,8 @@ f = map(l -> map(x -> parse(Int, x), split(l, "")), readlines("input"))
 ψ = (i, j) -> begin
     di = [1, -1, 0, 0]; dj = [0, 0, 1, -1]
     for i in 1:4
-        if (0 < i + di[i] && 0 < j + dj[i] && i + di[i] <= length(f) && j + dj[i] <= length(f[i]) && f[i + di[i]][j + dj[i]] <= f[i][j])
+        if 0 < i + di[i] && 0 < j + dj[i] && i + di[i] <= length(f) && j + dj[i] <= length(f[i])
+            
             return false
         end
     end
