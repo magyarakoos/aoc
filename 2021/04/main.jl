a6ed16d4d2971136bcb2b3498db43659af68cca4
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 10:35:01 +0200

    arch descktop sync: 2024-04-14 10:35:01

--- 2021/04/main.jl
+++ 2021/04/main.jl
@@ -2,7 +2,7 @@ f = open("input", "r")
 n::Vector{Int} = map(x -> parse(Int, x), split(readline(f), ","))
 while !eof(f)
     readline(f)
-    board = 
+    board = zeros(Int, 5, 5)
     for i in 1:5, j in 1:5
     
     end
