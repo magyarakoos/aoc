fc8da4d35b7c8fb418ab77f631778a311d683a90
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 11:12:26 +0200

    arch descktop sync: 2024-04-14 11:12:26

--- 2021/05/part1.jl
+++ 2021/05/part1.jl
@@ -1,4 +1,6 @@
 m = zeros(Int, 1000, 1000)
 where((x1, y1, x2, y2) -> x1 == x2 || y1 == y2, 
     map(l -> map(x -> (x[1], x[2], x[3], x[4]), 
-        match(r"(\d+),(\d+) -> (\d+),(\d+)", l)), readlines("input")))
+        match(r"(\d+),(\d+) -> (\d+),(\d+)", l)), 
+    readlines("input")
+))
