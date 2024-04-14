e02c0f20908500292c1401fbee70bcaf40de1bd6
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 12:24:25 +0200

    arch descktop sync: 2024-04-14 12:24:25

--- 2021/05/Program.cs
+++ 2021/05/Program.cs
@@ -1,45 +1,9 @@
-﻿List<Line> lines = new List<Line>();
-using (StreamReader f = new StreamReader("input")) {
-    while (!f.EndOfStream) {
-        int[] row = f.ReadLine().Split(' ').Select(int.Parse).ToArray();
-        if (row[0] == row[2] || row[1] == row[3] || Math.Abs(row[0] - row[2]) == Math.Abs(row[1] - row[3])) {
-            lines.Add(new Line { x1 = row[0], y1 = row[1], x2 = row[2], y2 = row[3] });
-        }
-    }
+﻿var input = File.ReadAllLines("input").Select(l => l.Split(" -> ").Select(s => s.Split(',').Select(int.Parse).ToList()).ToList()).Select(l => (x1: l[0][0], y1: l[0][1], x2: l[1][0], y2: l[1][1]));
+int[,] board1 = new int[input.Max(l => Math.Max(l.x1, l.x2)) + 1, input.Max(l => Math.Max(l.y1, l.y2)) + 1], board2 = new int[board1.GetLength(0), board1.GetLength(1)];
+foreach (var (x1, y1, x2, y2) in input)
+{
+    int sx = Math.Sign(x2 - x1), sy = Math.Sign(y2 - y1), n = Math.Max(Math.Abs(x1 - x2), Math.Abs(y1 - y2));
+    if (sx == 0 || sy == 0) for (int k = 0, i = x1, j = y1; k <= n; k++, i += sx, j += sy) board1[i, j]++;
+    for (int k = 0, i = x1, j = y1; k <= n; k++, i += sx, j += sy) board2[i, j]++;
 }
-int[,] map = new int[lines.Max(x => Math.Max(x.y1, x.y2)) + 1, lines.Max(x => Math.Max(x.x1, x.x2)) + 1];
-int more = 0;
-foreach (Line e in lines) {
-    if (e.x1 == e.x2) {
-        for (int i = Math.Min(e.y1, e.y2); i <= Math.Max(e.y1, e.y2); i++) {
-            if (++map[i, e.x1] == 2) {
-                ++more;
-            }
-        }
-    }
-    else if (e.y1 == e.y2) {
-        for (int i = Math.Min(e.x1, e.x2); i <= Math.Max(e.x1, e.x2); i++) {
-            if (++map[e.y1, i] == 2) {
-                ++more;
-            }
-        }
-    }
-    else {
-        int i = e.x1, j = e.y1;
-        while (i != e.x2) {
-            if (++map[j, i] == 2) {
-                ++more;
-            }
-            i += Math.Sign(e.x2 - e.x1);
-            j += Math.Sign(e.y2 - e.y1);
-        }
-        if (++map[j, i] == 2) {
-            ++more;
-        }
-    }
-}
-Console.WriteLine(more);
-
-class Line {
-    public int x1, x2, y1, y2;
-}
+Console.WriteLine($"{board1.Cast<int>().Count(n => n >= 2)}\n{board2.Cast<int>().Count(n => n >= 2)}");
