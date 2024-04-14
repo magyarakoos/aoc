b4d0e0137b84236a49e053cb259b68a779c4c45d
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 11:14:10 +0200

    arch descktop sync: 2024-04-14 11:14:10

--- 2021/05/part1.jl
+++ 2021/05/part1.jl
@@ -1,5 +1,7 @@
 m = zeros(Int, 1000, 1000)
-λ = ()
+λ = (x1, y1, x2, y2) -> begin
+    for in
+end
 map((x1, y1, x2, y2) -> λ(x1, y1, x2, y2), 
     where((x1, y1, x2, y2) -> x1 == x2 || y1 == y2, 
     map(l -> map(x -> (x[1], x[2], x[3], x[4]), 
