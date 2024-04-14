a832aa9ae000a86dab87eed2267c4e18f108f449
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 14:03:08 +0200

    arch descktop sync: 2024-04-14 14:03:08

--- 2021/08/part2.jl
+++ 2021/08/part2.jl
@@ -1,5 +1,5 @@
 using Combinatorics
-⊗ = (d) -> ∈(d, [[0,0,1,0,0,1,0],[1,0,1,0,0,1,0],[0,1,1,1,0,1,0],[]])
+⊗ = (d) -> ∈(d, [[1,1,1,0,1,1,1],[]])
 f = map(l -> [split(match(r"(.*)\|(.*)", l)[i]) for i in 1:2], readlines("input"))
 for (a, sol) in f
     for perm in permutations("abcdefg")
