688cb350b20b38b6648078071e3a39bdb8fb20b3
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 11:24:32 +0200

    arch descktop sync: 2024-04-14 11:24:32

--- 2021/05/part1.jl
+++ 2021/05/part1.jl
@@ -1,6 +1,6 @@
 m = zeros(Int, 1000, 1000)
-filter((x1, y1, x2, y2) -> x1 == x2 || y1 == y2, 
-map(x -> (x[1], x[2], x[3], x[4]),
+filter(p -> x1 == x2 || y1 == y2, 
+map(x -> [x[1], x[2], x[3], x[4]],
 map(l -> 
 map(x -> parse(Int, x), 
 match(r"(\d+),(\d+) -> (\d+),(\d+)", l)), 
