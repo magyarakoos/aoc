f72025f8b9586b789f89fcdb509c42715227bd13
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 08:35:20 +0200

    arch descktop sync: 2024-04-14 08:35:20

--- 2021/03/part2.jl
+++ 2021/03/part2.jl
@@ -10,5 +10,5 @@ end
 Ψ = (i, a, λ) -> begin
     if length(a) == 1 return a[1] end
     o = count(x -> (x >> i) & 1 == 1, a)
-    return solve(i - 1,)
+    return solve(i - 1, where(x -> x, a), λ)
 end
