a6277a4201f5ca8ac8b0e6e51cd5dcfd2caf9686
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 15:15:10 +0200

    arch descktop sync: 2024-04-14 15:15:10

--- 2021/09/part1.jl
+++ 2021/09/part1.jl
@@ -3,6 +3,6 @@ m = zeros(Int, length(f), length(f[1]))
 for i in eachindex(f), j in eachindex(f[1])
     m[i, j] = parse(Int, f[i][j])
 end
-for i in eachindex(f), j in eachline(f[1])
-    for di in []
+for i in eachindex(f), j in eachline(f[1]), di in [1, -1, 0, 0], dj in [0, 0, 1, -1]
+    if ()
 end
