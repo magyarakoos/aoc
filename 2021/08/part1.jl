d5c17770ee77a467ffdeda1ea944f632f98cda3c
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 13:34:31 +0200

    arch descktop sync: 2024-04-14 13:34:31

--- 2021/08/part1.jl
+++ 2021/08/part1.jl
@@ -1,1 +1,1 @@
-println(count(s -> in(length(s), [2, 3, 4, 7]), split(join(map(l -> match(r".*\|(.*)", l)[1], readlines("input")), " "))))
+println(count(s -> ∈(length(s), [2, 3, 4, 7]), split(join(map(l -> match(r".*\|(.*)", l)[1], readlines("input")), " "))))
