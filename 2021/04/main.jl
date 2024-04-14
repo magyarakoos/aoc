6964a41e98afe2b755f90b746f457897f4bd2e3c
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 10:36:44 +0200

    arch descktop sync: 2024-04-14 10:36:44

--- 2021/04/main.jl
+++ 2021/04/main.jl
@@ -1,9 +1,11 @@
 f = open("input", "r")
 n::Vector{Int} = map(x -> parse(Int, x), split(readline(f), ","))
+b = []
 while !eof(f)
     readline(f)
     board = []
     for i in 1:5
-        push!(board, map(x -> parse(Int, x), split("")))
+        push!(board, map(x -> parse(Int, x), split(readline(f))))
     end
+    push!(b, board)
 end
