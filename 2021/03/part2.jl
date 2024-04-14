62b21c70d7772b1f66da2946e370b3ec0d94bcec
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 08:53:07 +0200

    arch descktop sync: 2024-04-14 08:53:07

--- 2021/03/part2.jl
+++ 2021/03/part2.jl
@@ -8,10 +8,10 @@ for l in eachline(f)
     push!(a, x)
 end
 Σ = (i, a) -> o = count(x -> (x >> i) & 1 == 1, a)
-ω = (i, λ) -> i == -1 ? 0 : (λ(length(a), Σ(i, a)) << i) | ω(i - 1)
+ω = (i, λ) -> i == -1 ? 0 : (λ(length(a), Σ(i, a)) << i) | ω(i - 1, λ)
 Ψ = (i, a, λ) -> begin
     if length(a) == 1 return a[1] end
     return Ψ(i - 1, filter(x -> (x >> i) & 1 == λ(length(a), Σ(i, a)), a), λ)
 end
-println(ω(w - 1))
+println(ω(w - 1, ))
 println(Ψ(w - 1, a, (l, o) -> l <= o * 2) * Ψ(w - 1, a, (l, o) -> l > o * 2))
