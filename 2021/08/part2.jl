940300687ff6776a41760ed4dacf6cd708c97fea
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 14:40:59 +0200

    arch descktop sync: 2024-04-14 14:40:59

--- 2021/08/part2.jl
+++ 2021/08/part2.jl
@@ -2,7 +2,25 @@ using Combinatorics
 ⊗ = [[1,1,1,0,1,1,1],[0,0,1,0,0,1,0],[1,0,1,1,1,0,1],[1,0,1,1,0,1,1],[0,1,1,1,0,1,0],[1,1,0,1,0,1,1],[1,1,0,1,1,1,1],[1,0,1,0,0,1,0],[1,1,1,1,1,1,1],[1,1,1,1,0,1,1]] 
 f = map(l -> [split(match(r"(.*)\|(.*)", l)[i]) for i in 1:2], readlines("input"))
 ◿ = (a, perm) -> begin
-    
+    taken = fill("#", 10)
+    for s in a
+        mask = zeros(Int, 7)
+        for i in indexin(s, perm)
+            mask[i] = 1
+        end
+        if (!∈(mask, ⊗))
+            return false
+        end
+        n = findfirst(x -> x == mask, ⊗)
+        if taken[n] == "#"
+            taken[n] = s
+        else
+            if taken[n] != s
+                return false
+            end
+        end
+    end
+    return true
 end
 # for (a, sol) in f
 #     for perm in permutations("abcdefg")
@@ -15,24 +33,3 @@ end
 #         end
 #     end
 # end
-
-perm = ['d','e','a','f','g','b','c']
-perm = ['a','b','c','d','e','f','g']
-taken = fill("#", 10)
-for s in f[1][1]
-    mask = zeros(Int, 7)
-    for i in indexin(s, perm)
-        mask[i] = 1
-    end
-    if (!∈(mask, ⊗))
-        return false
-    end
-    n = findfirst(x -> x == mask, ⊗)
-    if taken[n] == "#"
-        taken[n] = s
-    else
-        if taken[n] != s
-            return false
-        end
-    end
-end
