5b297dcecddc77e1d04d50555cfe9df0c5436df1
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 11:41:09 +0200

    arch descktop sync: 2024-04-14 11:41:09

--- 2021/05/part1.jl
+++ 2021/05/part1.jl
@@ -1,5 +1,7 @@
 m = zeros(Int, 1000, 1000)
-map((p) -> for x in min(p[1], p[3]):max(p[1], p[3]), y in p[2]:p[4] m[y, x] += 1; println((x, y)) end, 
+map((p) -> for 
+        x in min(p[1], p[3]):max(p[1], p[3]), 
+        y in min(p[2], p[4]):max(p[2], p[4]) m[y, x] += 1 end, 
     filter(p -> true,#p[1] == p[3] || p[2] == p[4],
     map(l -> map(x -> parse(Int, x) + 1, 
         match(r"(\d+),(\d+) -> (\d+),(\d+)", l)), 
