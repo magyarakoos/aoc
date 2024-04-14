04f6298ac71d9719aa696bb6d4b0e41f314f08e9
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 12:58:11 +0200

    arch descktop sync: 2024-04-14 12:58:11

--- 2021/06/part1.jl
+++ 2021/06/part1.jl
@@ -1,5 +1,6 @@
 pop = zeros(Int, 9)
 map(x -> pop[parse(Int, x) + 1] += 1, split(readline("input"), ","))
+λ = (cyc)
 for i in 1:80
     cur = zeros(Int, 9)
     for j in 2:9 cur[j - 1] = pop[j] end
