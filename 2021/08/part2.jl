30b507a1b14e589ad1e560bb9a986b83c7d01e63
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 13:54:14 +0200

    arch descktop sync: 2024-04-14 13:54:14

--- 2021/08/part2.jl
+++ 2021/08/part2.jl
@@ -1,4 +1,8 @@
 using Combinatorics
-for (a, sol) in map(l -> [split(match(r"(.*)\|(.*)", l)[i]) for i in 1:2], readlines("input"))
+f = map(l -> [split(match(r"(.*)\|(.*)", l)[i]) for i in 1:2], readlines("input"))
+for (a, sol) in f
+    for perm in permutations("abcdefg")
+        
+    end
     println(a)
 end
