8393b896025a88fe1a32a0fb997e1691a8e3262f
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 07:40:35 +0200

    arch descktop sync: 2024-04-14 07:40:35

--- 2021/03/part2.cpp
+++ 2021/03/part2.cpp
@@ -9,8 +9,8 @@ int main() {
     while (f >> s) {
         if (oc.empty()) oc.resize(s.size());
         for (int i = 0; i < s.size(); i++) {
-            //v.back().push_back(s[i] == '1');
-            //oc[i] += v.back().back();
+            v.back().push_back(s[i] == '1');
+            oc[i] += v.back().back();
         }
         v.push_back({});
     }
