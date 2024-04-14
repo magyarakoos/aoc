9849c3d3d5aecbe8e4b21cfe88a696b63c4e230a
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 15:18:18 +0200

    arch descktop sync: 2024-04-14 15:18:18

--- 2021/09/part1.jl
+++ 2021/09/part1.jl
@@ -1,15 +1,12 @@
-f = map(l -> split(l, ""), readlines("input"))
-m = zeros(Int, length(f), length(f[1]))
+m = map(l -> split(l, ""), readlines("input"))
 ψ = (i, j) -> begin
     for di in [1, -1, 0, 0], dj in [0, 0, 1, -1]
-        if (0 < di && 0 < dj && di <= length(f) && dj <= length(f[1]) && m[di, dj] <= m[i, j])
+        if (0 < di && 0 < dj && di <= length(f) && dj <= length(f[1]) && m[di][dj] <= m[i][j])
             return false
         end
     end
     return true
 end
-for i in eachindex(f), j in eachindex(f[1])
-    m[i, j] = parse(Int, f[i][j])
-end
-for i in eachindex(f), j in eachline(f[1]), 
+for i in eachindex(f), j in eachline(f[1])
+    if 
 end
