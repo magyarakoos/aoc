17f5450a8ed548a2892ec84cab6d268b7e807304
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 07:44:45 +0200

    arch descktop sync: 2024-04-14 07:44:45

--- 2021/03/part2.cpp
+++ 2021/03/part2.cpp
@@ -1,25 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<vector<bool>> v({{}});
 
 void solve(int i) {
-    
+
 }
 
 int main() {
-    vector<vector<bool>> v({{}});
-    vector<int> oc;
     ifstream f("input");
     string s;
     while (f >> s) {
-        if (oc.empty()) oc.resize(s.size());
         for (int i = 0; i < s.size(); i++) {
             v.back().push_back(s[i] == '1');
-            oc[i] += v.back().back();
         }
         v.push_back({});
     }
     v.pop_back();
-    for (int& x : oc) x = x >= v.size() - x;
     auto [mn, mx] = minmax_element(v.begin(), v.end(),
         [&](auto a, auto b) {
             for (int i = 0; i < a.size(); i++) {
@@ -36,8 +32,4 @@ int main() {
             return false;
         }
     );
-    for (int x : *mn) cout << x;
-    cout << "\n";
-    for (int x : *mx) cout << x;
-    cout << "\n";
 }
