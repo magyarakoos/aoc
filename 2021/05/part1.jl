79857dd88298525e469f99b74731e9ba8e427da4
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 11:41:45 +0200

    arch descktop sync: 2024-04-14 11:41:45

--- 2021/05/part1.jl
+++ 2021/05/part1.jl
@@ -2,16 +2,10 @@ m = zeros(Int, 1000, 1000)
 map((p) -> for 
         x in min(p[1], p[3]):max(p[1], p[3]), 
         y in min(p[2], p[4]):max(p[2], p[4]) m[y, x] += 1 end, 
-    filter(p -> true,#p[1] == p[3] || p[2] == p[4],
+    filter(p -> p[1] == p[3] || p[2] == p[4],
     map(l -> map(x -> parse(Int, x) + 1, 
         match(r"(\d+),(\d+) -> (\d+),(\d+)", l)), 
         readlines("input")
     ))
 )
-for i in 1:10
-    for j in 1:10
-        print(m[i, j])
-    end
-    println()
-end
 println(count(x -> x >= 2, m))
