83cb5020a7c2fe3c132ce6378ef7c97a00e3ed97
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 13:07:34 +0200

    arch descktop sync: 2024-04-14 13:07:34

--- 2021/07/main.jl
+++ 2021/07/main.jl
@@ -1,4 +1,4 @@
-a = map(x -> parse(Int, x), split(readline("input"))); p1 = 0
+a = map(x -> parse(Int, x), split(readline("input"))); p1 = typemax(Int)
 for d in minimum(a):maximum(a)
-    p1 = min(p1, )
+    p1 = min(p1, sum(x -> abs(x - )))
 end
