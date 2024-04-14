56dfb779679a57c84961823352f56f3a9d650e58
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 14:39:14 +0200

    arch descktop sync: 2024-04-14 14:39:14

--- 2021/08/part2.jl
+++ 2021/08/part2.jl
@@ -1,6 +1,7 @@
 using Combinatorics
 ⊗ = [[1,1,1,0,1,1,1],[0,0,1,0,0,1,0],[1,0,1,1,1,0,1],[1,0,1,1,0,1,1],[0,1,1,1,0,1,0],[1,1,0,1,0,1,1],[1,1,0,1,1,1,1],[1,0,1,0,0,1,0],[1,1,1,1,1,1,1],[1,1,1,1,0,1,1]] 
 f = map(l -> [split(match(r"(.*)\|(.*)", l)[i]) for i in 1:2], readlines("input"))
+◿ = (a)
 # for (a, sol) in f
 #     for perm in permutations("abcdefg")
 #         taken = zeros(Char, 10)
@@ -16,7 +17,6 @@ f = map(l -> [split(match(r"(.*)\|(.*)", l)[i]) for i in 1:2], readlines("input"
 perm = ['d','e','a','f','g','b','c']
 perm = ['a','b','c','d','e','f','g']
 taken = fill("#", 10)
-
 for s in f[1][1]
     mask = zeros(Int, 7)
     for i in indexin(s, perm)
