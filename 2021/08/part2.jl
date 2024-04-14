17c1605e8068564b7bb7cc3e741b2fcdaf48ed06
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-14 14:08:44 +0200

    arch descktop sync: 2024-04-14 14:08:44

--- 2021/08/part2.jl
+++ 2021/08/part2.jl
@@ -4,7 +4,9 @@ f = map(l -> [split(match(r"(.*)\|(.*)", l)[i]) for i in 1:2], readlines("input"
 for (a, sol) in f
     for perm in permutations("abcdefg")
         taken = zeros(Char, 10)
-        
+        for s in a
+            
+        end
     end
 end
 
