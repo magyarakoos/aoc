5d426d5b09849fe892e435ef3ee8d3994f80699a
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-15 19:51:50 +0200

    arch descktop sync: 2024-04-15 19:51:50

--- 2021/11/part1.jl
+++ 2021/11/part1.jl
@@ -1,7 +1,9 @@
 f = map(l -> map(x -> parse(Int, x), split(l, "")), readlines("input"))
 ψ = (i, j) -> begin
+    f[i][j] = 0
     for ni in (i - 1):(i + 1), nj in (j - 1):(j + 1)
         if ni == i && nj == j continue end
         f[ni][nj] += 1
-        
+        if f[ni][nj] > 9
+            ψ
 end
