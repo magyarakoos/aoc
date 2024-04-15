a8de5a10f1317156fd244cc7f2e812e8e2f44055
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-15 19:59:06 +0200

    arch descktop sync: 2024-04-15 19:59:06

--- 2021/11/part1.jl
+++ 2021/11/part1.jl
@@ -2,7 +2,7 @@ f = map(l -> map(x -> parse(Int, x), split(l, "")), readlines("input"))
 vis = Set(); p1 = 0
 ψ = (i, j) -> begin
     f[i][j] = 0
-    p1 += 1
+    global p1 += 1
     for ni in (i - 1):(i + 1), nj in (j - 1):(j + 1)
         if (ni == i && nj == j) || (ni, nj) ∈ vis ||
             ni < 1 || nj < 1 || ni > length(f) || nj > length(f[1])
@@ -15,13 +15,11 @@ vis = Set(); p1 = 0
         end
     end
 end
-for _ in 1:3
+for _ in 1:10
     global vis = Set()
     global f = [l .+ 1 for l in f]
     for i in eachindex(f), j in eachindex(f[1])
         if f[i][j] > 9 ψ(i, j) end
     end
-    for l in f println(l) end
-    println()
 end
-
+println(p1)
