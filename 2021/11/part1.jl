916c29ee01248567712a2c907591147d892eef7e
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-15 20:24:47 +0200

    arch descktop sync: 2024-04-15 20:24:47

--- 2021/11/part1.jl
+++ 2021/11/part1.jl
@@ -2,7 +2,7 @@ f = map(l -> map(x -> parse(Int, x), split(l, "")), readlines("input"))
 vis = zeros(Int, length(f), length(f[1]))
 p1 = 0; curr = 0
 ψ = (i, j) -> begin
-    if ni < 1 || nj < 1 || ni > length(f) || nj > length(f[1]) ||
+    if i < 1 || nj < 1 || ni > length(f) || nj > length(f[1]) ||
        vis[ni][nj] == curr return end
     f[i][j] += 1
     if f[i][j] <= 9 return end
@@ -13,9 +13,8 @@ p1 = 0; curr = 0
         ψ(ni, nj)
     end
 end
-for _ in 1:10
+for _ in 1:100
     global curr += 1
-    global f = [l .+ 1 for l in f]
     for i in eachindex(f), j in eachindex(f[1])
         if f[i][j] > 9 ψ(i, j) end
     end
