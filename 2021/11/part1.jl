fd5d9dec69178f913269a0ccea73657d348c15a2
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-15 19:51:16 +0200

    arch descktop sync: 2024-04-15 19:51:16

--- 2021/11/part1.jl
+++ 2021/11/part1.jl
@@ -2,5 +2,6 @@ f = map(l -> map(x -> parse(Int, x), split(l, "")), readlines("input"))
 ψ = (i, j) -> begin
     for ni in (i - 1):(i + 1), nj in (j - 1):(j + 1)
         if ni == i && nj == j continue end
+        f[ni][nj] += 1
         
 end
