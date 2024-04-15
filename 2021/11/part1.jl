1e1815c23eacabcb81da31887d84562b2541c371
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-15 20:27:01 +0200

    arch descktop sync: 2024-04-15 20:27:01

--- 2021/11/part1.jl
+++ 2021/11/part1.jl
@@ -13,6 +13,7 @@ p1 = 0; curr = 0
         ψ(ni, nj)
     end
 end
+@time begin
 for _ in 1:100
     global curr += 1
     for i in eachindex(f), j in eachindex(f[1])
@@ -20,45 +21,3 @@ for _ in 1:100
     end
 end
 println(p1)
-#include <bits/stdc++.h>
-using namespace std;
-#define N 10
-#define K 10
-int board[N][N];
-int flashed[N][N];
-int flashedMarker = 1;
-int res = 0;
-void boop(int i, int j) {
-    if (i < 0 || j < 0 || i >= N || j >= N || flashed[i][j] == flashedMarker) return;
-    board[i][j]++;
-    if (board[i][j] < K) return;
-    flashed[i][j] = flashedMarker;
-    res++;
-    board[i][j] = 0;
-    for (int di = -1; di <= 1; di++) {
-        for (int dj = -1; dj <= 1; dj++) {
-            boop(i + di, j + dj);
-        }
-    }
-
-}
-int main() {
-    ifstream f(".input.txt");
-    string l;
-    for (int i = 0; i < N; i++) {
-        f >> l;
-        for (int j = 0; j < N; j++) {
-            board[i][j] = l[j] - '0';
-        }
-    }
-    f.close();
-    for (int i = 0; i < 100; i++) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                boop(i, j);
-            }
-        }
-        flashedMarker++;
-    }
-    cout << res;
-}
