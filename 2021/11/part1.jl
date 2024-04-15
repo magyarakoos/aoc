7b195b9011a2be5aceb9bacdd57fd7eb937d8bef
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-15 20:34:18 +0200

    arch descktop sync: 2024-04-15 20:34:18

--- 2021/11/part1.jl
+++ 2021/11/part1.jl
@@ -1,12 +1,12 @@
 f = map(l -> map(x -> parse(Int, x), split(l, "")), readlines("input"))
 vis = zeros(Int, length(f), length(f[1]))
-p1 = 0; label = 0; curr = 0
+p1 = 0; curr = 0
 ψ = (i, j) -> begin
     if i < 1 || j < 1 || i > length(f) || j > length(f[1]) ||
-       vis[i, j] == label return end
+       vis[i, j] == curr return end
     f[i][j] += 1
     if f[i][j] <= 9 return end
-    vis[i, j] = label
+    vis[i, j] = curr
     global p1 += 1
     f[i][j] = 0
     for ni in (i - 1):(i + 1), nj in (j - 1):(j + 1)
@@ -14,11 +14,9 @@ p1 = 0; label = 0; curr = 0
     end
 end
 while true
-    global label += 1
+    global curr += 1
     for i in eachindex(f), j in eachindex(f[1])
         ψ(i, j)
     end
-    global p1 += curr
-    if curr == 100 println(label); exit(0) end
-    if label == 100 println(p1) end
+    if curr == 100 println(p1) end
 end
