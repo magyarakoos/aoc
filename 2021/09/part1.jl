e6f00af7d07fede0ef22794eba9be55fea4032a9
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-15 13:29:51 +0200

    arch descktop sync: 2024-04-15 13:29:51

--- 2021/09/part1.jl
+++ 2021/09/part1.jl
@@ -30,5 +30,5 @@ for i in eachindex(basins)
     φ(basins[i][1], basins[i][2], i)
 end
 println(sum(x -> f[x[1]][x[2]], basins) + length(basins))
-println(sort(filter(x -> x != 0, collect(values(countmap(vis[:])))),rev=true)[1:3])
+println(countmap(vis[:]))
 display(vis)
