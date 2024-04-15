663c844f0b2364d2e9f0db0bfd2a51dd80ee1f16
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-15 13:44:57 +0200

    arch descktop sync: 2024-04-15 13:44:57

--- 2021/10/main.jl
+++ 2021/10/main.jl
@@ -1,6 +1,8 @@
 d = Dict('('=>')', '['=>']', '{'=>'}', '<'=>'>')
+v = Dict(')'=>, '['=>']', '{'=>'}', '<'=>'>')
+p1 = 0
 for l in readlines("input")
     s = Stack{Char}()    
     for c in l
         if c ∈ keys(d) push!(s, c) end
-        if c != d[first(s)]
+        if c != d[first(s)] global p1 += 
