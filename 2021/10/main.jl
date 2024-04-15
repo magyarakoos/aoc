d5ccbb2441ac63daf6a648f6c2199c99835ca04e
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-15 19:41:14 +0200

    arch descktop sync: 2024-04-15 19:41:14

--- 2021/10/main.jl
+++ 2021/10/main.jl
@@ -4,13 +4,16 @@ v = Dict(')'=>3, ']'=>57, '}'=>1197, '>'=>25137)
 v2 = Dict('('=>1, '['=>2, '{'=>3, '<'=>4)
 p1 = 0; p2 = []
 for l in readlines("input")
-    s = Stack{Char}()  
+    s = Stack{Char}(); corrupt = false
     for c in l
         if c ∈ keys(d) push!(s, c)
-        elseif c != d[first(s)] global p1 += v[c]; break
+        elseif c != d[first(s)]
+            global p1 += v[c]
+            corrupt = true
+            break
         else pop!(s) end
     end
-    if length(s) > 0
+    if !corrupt && length(s) > 0
         score = 0
         while length(s) > 0
             score = score * 5 + v2[first(s)]
