2b42ca0345a8d05c20618c5abea8f405c7bcdec4
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-15 13:49:03 +0200

    arch descktop sync: 2024-04-15 13:49:03

--- 2021/10/main.jl
+++ 2021/10/main.jl
@@ -1,6 +1,7 @@
 using DataStructures
 d = Dict('('=>')', '['=>']', '{'=>'}', '<'=>'>')
 v = Dict(')'=>3, ']'=>57, '}'=>1197, '>'=>25137)
+println(d)
 p1 = 0
 for l in readlines("input")
     s = Stack{Char}()    
