c6f2cb10b864b4124464ff17d74a26479d826bdf
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-15 20:20:17 +0200

    arch descktop sync: 2024-04-15 20:20:17

--- 2021/11/part1.jl
+++ 2021/11/part1.jl
@@ -1,11 +1,12 @@
 f = map(l -> map(x -> parse(Int, x), split(l, "")), readlines("input"))
 vis = zeros(Int, length(f), length(f[1]))
-p1 = 0
+p1 = 0; curr = 0
 ψ = (i, j) -> begin
     f[i][j] = 0
     global p1 += 1
     for ni in (i - 1):(i + 1), nj in (j - 1):(j + 1)
-        if ni < 1 || nj < 1 || ni > length(f) || nj > length(f[1])
+        if ni < 1 || nj < 1 || ni > length(f) || nj > length(f[1]) ||
+           (ni == i && nj == j) || vis[ni][nj] == curr
             continue
         end
         f[ni][nj] += 1
