7aefd158f7b39536e22fb2a2045007015118300f
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-15 13:51:56 +0200

    arch descktop sync: 2024-04-15 13:51:56

--- 2021/10/main.jl
+++ 2021/10/main.jl
@@ -7,7 +7,7 @@ for l in readlines("input")
     s = Stack{Char}()    
     for c in l
         if c ∈ keys(d) push!(s, c) end
-        if c != d[first(s)] global p1 += v[c]; break end
+        if c != d[first(s)] global p1 += 1; break end
         push!(s, c)
     end
 end
