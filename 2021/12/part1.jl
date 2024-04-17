d6a69066d2ac51baeac14524093fc39bf7474bc8
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-17 09:04:21 +0200

    arch descktop sync: 2024-04-17 09:04:21

--- 2021/12/part1.jl
+++ 2021/12/part1.jl
@@ -1,18 +1,20 @@
 using DataStructures
 g = Dict{String, Vector{String}}()
-vis = Set(); ans = [0, 0]; twice = false
+vis = Dict{String, Int}(); ans = [0, 0]
 dfs = (u, i) -> begin
     if u == "end"
         ans[i] += 1
         return
     end
     if islowercase(u[1])
-        push!(vis, u)
+        vis[u] += 1
     end
     for v in g[u]
-        
+        if vis[v] < i && v != "start" dfs(v) end
     end
-    delete!(vis, u)
+    if islowercase(u[1])
+        vis[u] -= 1
+    end
 end
 for l in eachline("input")
     v = split(l, "-")
@@ -25,6 +27,5 @@ for l in eachline("input")
     push!(g[v[2]], v[1])
     vis[v[1]] = 0; vis[v[2]] = 0
 end
-vis["start"] = 1
-dfs("start", 1); dfs("start", 2)
-println("$(ans[1])\n$(ans[2])")
+dfs("start")
+println(p1)
