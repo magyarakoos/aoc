f7a713049a121ed649fbe2e5cde242cb8c631ae8
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-17 08:54:25 +0200

    arch descktop sync: 2024-04-17 08:54:25

--- 2021/12/part1.jl
+++ 2021/12/part1.jl
@@ -1,9 +1,9 @@
 using DataStructures
 g = Dict{String, Vector{String}}()
-vis = Dict{String, Int}(); ans = []
+vis = Dict{String, Int}(); ans = [0, 0]
 dfs = (u, i) -> begin
     if u == "end"
-        global p1 += 1
+        ans[i] += 1
     end
     if islowercase(u[1])
         vis[u] += 1
@@ -24,6 +24,7 @@ for l in eachline("input")
     end
     push!(g[v[1]], v[2])
     push!(g[v[2]], v[1])
+    vis[v[1]] = 0; vis[v[2]] = 0
 end
 dfs("start")
 println(p1)
