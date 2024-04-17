29983e3d91d899229b2541906aae0c2e0e06cac0
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-17 19:05:59 +0200

    arch descktop sync: 2024-04-17 19:05:59

---
+++ 2021/12/part2.jl
@@ -0,0 +1,36 @@
+using DataStructures
+g = Dict{String, Vector{String}}()
+vis = Set(); twice = false; p2 = 0
+dfs = (u) -> begin
+    if u == "end"
+        delete!(vis, "end")
+        global p2 += 1
+        return
+    end
+    if islowercase(u[1])
+        push!(vis, u)
+    end
+    for v in g[u]
+        if !(v ∈ vis)
+            dfs(v)
+        elseif !(v ∈ ["start", "end"]) && !twice
+            global twice = true
+            push!(vis, v)
+            dfs(v)
+            global twice = false
+        end
+    end
+    delete!(vis, u)
+end
+for l in eachline("input")
+    v = split(l, "-")
+    for x in v
+        if !haskey(g, x)
+            g[x] = []
+        end
+    end
+    push!(g[v[1]], v[2])
+    push!(g[v[2]], v[1])
+end
+dfs("start")
+println(p2)
