562d080969a428b051e92283ca33b38d077ec2af
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 13:34:20 +0200

    arch descktop sync: 2024-04-18 13:34:20

--- 2021/15/part1.jl
+++ 2021/15/part1.jl
@@ -1,9 +1,9 @@
 using DataStructures
 m = map(l -> split(l, ""), readlines("input"))
 dx = [1, -1, 0, 0]; dy = [0, 0, 1, -1]
-q = BinaryMinHeap{Tuple{Int, Int}}()
-push!(q, (0, 0))
+q = BinaryMinHeap{Vector{Int}}()
+push!(q, (0, 0, 0))
 while !isempty(q)
     (dist, u) = pop!(q)
-    
+
 end
