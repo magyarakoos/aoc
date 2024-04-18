e7406c6a2543d44bb1058823ae2262cdcda057b9
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 09:53:11 +0200

    arch descktop sync: 2024-04-18 09:53:11

--- 2021/15/part1.jl
+++ 2021/15/part1.jl
@@ -2,4 +2,7 @@ using DataStructures
 m = map(l -> split(l, ""), readlines("input"))
 q = PriorityQueue{Tuple{Int, Int}}()
 for i in 1:10
-    
+    enqueue!(q, i)
+end
+while length(q) > 0
+    println(peek(q))
