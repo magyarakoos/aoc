3068869f537796a09c0289d01e9f338ea1ede6ed
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 13:30:12 +0200

    arch descktop sync: 2024-04-18 13:30:12

--- 2021/15/part1.jl
+++ 2021/15/part1.jl
@@ -1,9 +1,9 @@
 using DataStructures
 m = map(l -> split(l, ""), readlines("input"))
+q = BinaryMinHeap(Int)
 for i in 1:10
     push!(q, i)
 end
 while length(q) > 0
-    println(peek(q))
-    dequeue!(q)
+    println(pop!(q))
 end
