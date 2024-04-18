757ead0ec6f375a3b105aa5084902e689a9df915
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 13:49:39 +0200

    arch descktop sync: 2024-04-18 13:49:39

--- 2021/15/part1.jl
+++ 2021/15/part1.jl
@@ -1,7 +1,7 @@
 using DataStructures
 m = map(l -> split(l, ""), readlines("input"))
 dx = [1, -1, 0, 0]; dy = [0, 0, 1, -1]; n = length(m)
-q = BinaryMinHeap{Tuple{Int, Int, Int}}()
+q = BinaryMinHeap{Tuple{Int, Int, Int}}(); dist = Dict()
 push!(q, (0, 0, 0))
 while !isempty(q)
     (dist, x, y) = pop!(q)
@@ -11,6 +11,7 @@ while !isempty(q)
     end
     for i in 1:4
         nx = x + dx[i], ny = y + dy[i]
-        if nx < 1 || ny < 1 || nx 
+        if nx < 1 || ny < 1 || nx > n || ny > n continue end
+        if 
     end
 end
