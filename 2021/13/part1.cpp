fe537b37f0c62c25f2ea157cc1d0b2ba46601075
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 07:55:04 +0200

    arch descktop sync: 2024-04-18 07:55:04

--- 2021/13/part1.cpp
+++ 2021/13/part1.cpp
@@ -16,7 +16,7 @@ int main() {
         if (s[11] == 'y') {
             for (int l = 0, r = v * 2; l < r; l++, r--) {
                 for (int x = 0; x < MAXN; x++) {
-                    
+                    m[l][x] |= 
                 }
             }
         } else {
