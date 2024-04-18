150ac110437ef789a99114081673226268bba690
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 15:57:23 +0200

    arch descktop sync: 2024-04-18 15:57:23

--- 2021/15/part1.jl
+++ 2021/15/part1.jl
@@ -22,11 +22,8 @@ end
 m = map(l -> map(x -> parse(Int, x), split(l, "")), readlines("input"))
 println(dijkstra(m))
 n = length(m)
-for i in 0:3, j in 1:n, k in 1:n
+for i in 1:4, j in 1:n, k in 1:n
     push!(m[j], (m[j][k] + i) % 9 + 1)
-end
-for i in 0:4, j in 1:n
-    push!(m, map(x -> (x + i - 1) % 9 + 1, m[j]))
 end
 println(dijkstra(m))
 for l in m
