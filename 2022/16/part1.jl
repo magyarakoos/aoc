551d2c6827f1de4f2b6821a49c923548e2272300
magyarakoos <ninjaoffical08@gmail.com>
2024-04-18 18:41:06 +0200

    arch descktop sync: 2024-04-18 18:41:06

--- 2022/16/part1.jl
+++ 2022/16/part1.jl
@@ -1,5 +1,6 @@
 f = map(l -> match(r".{6}(.[A-Z]).{15}(\d+).{24}\s+(.*)", l), readlines("input"))
 rates = Dict{String, Int}(); g = Dict{String, Vector{String}}()
 for m in f
-    rates[m[1]] = parse(Int, m[2]);
+    rates[m[1]] = parse(Int, m[2])
+    for v in split(m[3], ", ")
 end
