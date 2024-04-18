b01b549b5909efd6e21c6c5b23690e485a1be112
magyarakoos <ninjaoffical08@gmail.com>
2024-04-18 19:07:02 +0200

    arch descktop sync: 2024-04-18 19:07:02

--- 2022/16/part1.jl
+++ 2022/16/part1.jl
@@ -1,19 +1,20 @@
 using Combinatorics
 f = map(l -> match(r".{6}(.[A-Z]).{15}(\d+).{24}\s+(.*)", l), readlines("input"))
 rates = Dict{String, Int}(); g = Dict{String, Vector{String}}()
-visit = Vector{String}()
+visit = Vector{String}(); nodes = Vector{String}
 for m in f
     rates[m[1]] = parse(Int, m[2])
     g[m[1]] = Vector{String}()
     for v in split(m[3], ", ")
         push!(g[m[1]], v)
     end
+    push!(nodes, m[1])
     if rates[m[1]] != 0
         push!(visit, m[1])
     end
 end
 
-# floyd-warshall on non-zero width pipes O(n^3)
+# floyd-warshall
 n = length(visit)
 dist = Dict{String, Dict{String, Int}}()
 for u in visit
