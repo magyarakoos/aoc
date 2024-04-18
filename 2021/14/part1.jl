826764feba09170d743ce5cce5578ee09909a185
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 08:27:42 +0200

    arch descktop sync: 2024-04-18 08:27:42

--- 2021/14/part1.jl
+++ 2021/14/part1.jl
@@ -2,3 +2,8 @@ f = open("input", "r")
 s = readline(f); readline(f)
 d = Dict{String, Char}()
 map(l -> d[l[1:2]] = l[7], eachline(f))
+for _ in 1:10
+    t = 1
+    for i in 2:length(s)
+    end
+end
