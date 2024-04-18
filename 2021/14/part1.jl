4ae6a964f7707c3b93f139091d557c7aad091979
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 08:56:46 +0200

    arch descktop sync: 2024-04-18 08:56:46

--- 2021/14/part1.jl
+++ 2021/14/part1.jl
@@ -5,6 +5,8 @@ map(l -> d[l[1:2]] = l[7], eachline(f))
 for _ in 1:10
     t = 1
     for i in 2:length(s)
-        if haskey(d, s[])
+        t += 
+        if haskey(d, s[(i - 1):i])
+
     end
 end
