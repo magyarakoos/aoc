f19010556b07e5bbdbfb85123f430273fc50e1f0
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 09:31:16 +0200

    arch descktop sync: 2024-04-18 09:31:16

--- 2021/14/part2.jl
+++ 2021/14/part2.jl
@@ -3,17 +3,4 @@ f = open("input", "r")
 s = readline(f); readline(f)
 d = Dict{String, Char}()
 map(l -> d[l[1:2]] = l[7], eachline(f))
-for _ in 1:40
-    t = s[1]
-    for i in eachindex(s)
-        if i > 1
-            if haskey(d, s[i - 1:i])
-                t *= d[s[i - 1:i]]
-            end
-            t *= s[i]
-        end
-    end
-    global s = t
-end
-s = sort(collect(values(countmap(s))))
-println(s[end] - s[1])
+
