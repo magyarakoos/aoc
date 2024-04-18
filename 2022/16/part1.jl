66b96760672ed8dda06d5af10cad6e8a3121301e
magyarakoos <ninjaoffical08@gmail.com>
2024-04-18 18:39:17 +0200

    arch descktop sync: 2024-04-18 18:39:17

--- 2022/16/part1.jl
+++ 2022/16/part1.jl
@@ -1,2 +1,4 @@
-f = map(l -> map(m -> [m[1], parse(Int, m[2])], match(r".{6}(.[A-Z]).{15}(\d+).{24}\s+(.*)", l)), readlines("input"))
-println(f)
+f = map(l -> match(r".{6}(.[A-Z]).{15}(\d+).{24}\s+(.*)", l), readlines("input"))
+for m in f
+    println(m)
+end
