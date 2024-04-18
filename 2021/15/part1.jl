dbb011cc54db84fb2ff43796ea5ef1305059c202
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 13:53:13 +0200

    arch descktop sync: 2024-04-18 13:53:13

--- 2021/15/part1.jl
+++ 2021/15/part1.jl
@@ -4,7 +4,7 @@ dx = [1, -1, 0, 0]; dy = [0, 0, 1, -1]; n = length(m)
 q = BinaryMinHeap{Tuple{Int, Int, Int}}()
 dist = fill(typemax(Int), n, n)
 push!(q, (0, 1, 1))
-dist[1][1] = 0
+dist[1,1] = 0
 while !isempty(q)
     (d, x, y) = pop!(q)
     if x == n && y == m
@@ -12,7 +12,7 @@ while !isempty(q)
         exit(0)
     end
     for i in 1:4
-        nx = x + dx[i], ny = y + dy[i]
+        nx = x + dx[i]; ny = y + dy[i]
         if nx < 1 || ny < 1 || nx > n || ny > n continue end
         if d + m[ny][nx] < dist[ny,nx]
             dist[ny,nx] = d + m[ny][nx]
