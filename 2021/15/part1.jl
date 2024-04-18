3c1ce0ecddfb07a2570c2a7930e092b37d4110d1
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 13:50:18 +0200

    arch descktop sync: 2024-04-18 13:50:18

--- 2021/15/part1.jl
+++ 2021/15/part1.jl
@@ -1,7 +1,8 @@
 using DataStructures
 m = map(l -> split(l, ""), readlines("input"))
 dx = [1, -1, 0, 0]; dy = [0, 0, 1, -1]; n = length(m)
-q = BinaryMinHeap{Tuple{Int, Int, Int}}(); dist = Dict()
+q = BinaryMinHeap{Tuple{Int, Int, Int}}()
+dist = fill(typemax(Int), n, n)
 push!(q, (0, 0, 0))
 while !isempty(q)
     (dist, x, y) = pop!(q)
