ae5bfa51bdab66782249e8efbe86cbae073e3945
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 09:02:32 +0200

    arch descktop sync: 2024-04-18 09:02:32

--- 2021/14/part1.jl
+++ 2021/14/part1.jl
@@ -3,11 +3,11 @@ s = readline(f); readline(f)
 d = Dict{String, Char}()
 map(l -> d[l[1:2]] = l[7], eachline(f))
 for _ in 1:10
-    t::String = s[1]
+    t = s[1]
     for i in 2:length(s)
         if haskey(d, s[(i - 1):i])
-            t = t + d[s[(i - 1):i]]
+            t *= d[s[(i - 1):i]]
         end
-        t = t + s[i]
+        t *= s[i]
     end
 end
