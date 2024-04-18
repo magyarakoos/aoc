da2f24e17c80ee32e9049a6244e9bad91c4f420f
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 08:27:03 +0200

    arch descktop sync: 2024-04-18 08:27:03

--- 2021/14/part1.jl
+++ 2021/14/part1.jl
@@ -1,9 +1,4 @@
 f = open("input", "r")
-s = readline(f)
-readline(f)
+s = readline(f); readline(f)
 d = Dict{String, Char}()
-for l in eachline(f)
-    d[l[1:2]] = l[7]
-end
-map(l -> )
-println(d)
+map(l -> d[l[1:2]] = l[7], eachline(f))
