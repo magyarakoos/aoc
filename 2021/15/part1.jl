476760f3863d67a7eaf7f998afbf4c82349b2250
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 15:51:22 +0200

    arch descktop sync: 2024-04-18 15:51:22

--- 2021/15/part1.jl
+++ 2021/15/part1.jl
@@ -26,8 +26,7 @@ for i in 1:5, j in 1:n, k in 1:n
     push!(m[j], m[j][k] + i)
 end
 for i in 1:5, j in 1:n
-    push!(m, [])
-    for k in 1:n
+    push!(m, m[j]
 end
 println(dijkstra(m))
 for l in m
