7d08e7823d4e40652a3e013241b067f8f0519c7b
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 09:04:18 +0200

    arch descktop sync: 2024-04-18 09:04:18

--- 2021/14/part1.jl
+++ 2021/14/part1.jl
@@ -4,7 +4,8 @@ d = Dict{String, Char}()
 map(l -> d[l[1:2]] = l[7], eachline(f))
 for _ in 1:10
     t = s[1]
-    for i in 2:length(s)
+    for i in eachindex(s)
+        if i > 1
         if haskey(d, s[(i - 1):i])
             t *= d[s[(i - 1):i]]
         end
