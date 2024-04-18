8fbb99132b7c21371028d80edd4229fc4a78f040
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 09:25:40 +0200

    arch descktop sync: 2024-04-18 09:25:40

---
+++ 2021/14/part2.jl
@@ -0,0 +1,21 @@
+using StatsBase
+f = open("input", "r")
+s = readline(f); readline(f)
+d = Dict{String, Char}()
+map(l -> d[l[1:2]] = l[7], eachline(f))
+for _ in 1:40
+    @time begin
+        t = s[1]
+        for i in eachindex(s)
+            if i > 1
+                if haskey(d, s[i - 1:i])
+                    t *= d[s[i - 1:i]]
+                end
+                t *= s[i]
+            end
+        end
+        global s = t
+    end
+end
+s = sort(collect(values(countmap(s))))
+println(s[end] - s[1])
