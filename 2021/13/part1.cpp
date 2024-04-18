b8ffb587cf3385baf02a4f8a410c57e0f343383a
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 07:42:07 +0200

    arch descktop sync: 2024-04-18 07:42:07

--- 2021/13/part1.cpp
+++ 2021/13/part1.cpp
@@ -5,16 +5,16 @@ int main() {
     ifstream f("input");
     string s;
     vector<array<int, 2>> ptS;
-    while (getline(f, s)) {
+    while (getline(f, s) && s != "") {
         auto d = s.find(',');
         ptS.push_back({stoi(s.substr(0, d)), stoi(s.substr(d + 1))});
     }
-    // vector<array<int, 2>> foldS;
-    // while (f >> s) {
-    //     foldS.push_back({s[11] == 'y', stoi(s.substr(13))});
-    // }
+    vector<array<int, 2>> foldS;
+    while (getline(f, s)) {
+        foldS.push_back({s[11] == 'y', stoi(s.substr(13))});
+    }
     
     for (auto [x, y] : ptS) cout << x << " " << y << "\n";
-    //cout << "\n";
-    //for (auto [t, v] : foldS) cout <<  t << " " << v << "\n";
+    cout << "\n";
+    for (auto [t, v] : foldS) cout <<  t << " " << v << "\n";
 }
