6cd3adf20e43f695bd0d9e6a115d2c947fbe19b6
magyarakoos <ninjaoffical08@gmail.com>
2024-04-18 20:50:06 +0200

    arch descktop sync: 2024-04-18 20:50:06

--- 2022/16/part1.jl
+++ 2022/16/part1.jl
@@ -57,11 +57,10 @@ count = 0
 # optimized backtrack to find all (valid) permutations
 # when we run out of time, we cut off the search
 backtrack = (order, t) -> begin
-    println(count)
-    sleep(0.1)
     if length(order) == m
         println(order)   
         global count += 1
+        println(count)
         return
     end
     for u in visit
