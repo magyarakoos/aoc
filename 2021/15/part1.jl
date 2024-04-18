6b42f984f949fed7d81e286a873304b7ccce7eab
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 13:31:19 +0200

    arch descktop sync: 2024-04-18 13:31:19

--- 2021/15/part1.jl
+++ 2021/15/part1.jl
@@ -1,9 +1,3 @@
 using DataStructures
 m = map(l -> split(l, ""), readlines("input"))
-q = BinaryMinHeap(Int)
-for i in 1:10
-    push!(q, i)
-end
-while !isempty(q)
-    println(pop!(q))
-end
+q = BinaryMinHeap{Int}()
