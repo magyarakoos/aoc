8b68d41d1bc4deac6df295d3811c6e46251c57d0
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-18 09:36:37 +0200

    arch descktop sync: 2024-04-18 09:36:37

--- 2021/14/part2.jl
+++ 2021/14/part2.jl
@@ -1,5 +1,5 @@
 using StatsBase
 f = open("input", "r")
 s = readline(f); readline(f)
-d = Dict{String, Char}()
+d = Dict{String, Char}(); cache = Dict{String, Int}()
 map(l -> d[l[1:2]] = l[7], eachline(f))
