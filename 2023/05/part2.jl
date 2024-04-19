27f3e25e567dfd10da68c3c086c1dd3a7780cdb5
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-19 08:35:53 +0200

    arch descktop sync: 2024-04-19 08:35:53

--- 2023/05/part2.jl
+++ 2023/05/part2.jl
@@ -3,4 +3,6 @@ f = open("input", "r")
 start = Vector{Interval{Closed, Closed}}()
 m = map(x -> parse(Int, x), split(match(r"\S+(.*)", readline(f))[1]))
 
-for i in 1:10:
+for i in 2:2:length(m)
+    println(m[i - 1])
+end
