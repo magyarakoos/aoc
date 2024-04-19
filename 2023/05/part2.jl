5c47724c6b086445b6c093dbe9c23ef2763533b1
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-19 10:33:55 +0200

    arch descktop sync: 2024-04-19 10:33:55

--- 2023/05/part2.jl
+++ 2023/05/part2.jl
@@ -21,11 +21,10 @@ while !eof(f)
     end
 
     if !eof(f)
-    
+        push!(source, Interval{Closed, Open}(0, minimum(map(x -> first(x), source))))
+        push!(source, Interval{Closed, Open}(maximum(map(x -> last(x), source)), typemax(Int)))
+        for _ in 1:2 push!(diff, 0) end
     end
-    push!(source, Interval{Closed, Open}(0, minimum(map(x -> first(x), source))))
-    push!(source, Interval{Closed, Open}(maximum(map(x -> last(x), source)), typemax(Int)))
-    for _ in 1:2 push!(diff, 0) end
 
     curr = Vector{Interval{Int, Closed, Open}}()
     for intv in start
