850f0a43514cae18bcfb46e63d999a7d239a3266
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-19 11:23:00 +0200

    arch descktop sync: 2024-04-19 11:23:00

--- 2023/05/part2.jl
+++ 2023/05/part2.jl
@@ -21,10 +21,11 @@ while !eof(f)
         push!(diff, l[1] - l[2])
     end
 
+    # add unmapped regions as zero-diff intervals
     push!(source, Interval{Closed, Open}(0, minimum(map(x -> first(x), source))))
     push!(source, Interval{Closed, Open}(maximum(map(x -> last(x), source)), typemax(Int)))
     for _ in 1:2 push!(diff, 0) end
-
+    
     curr = Vector{Interval{Int, Closed, Open}}()
     for intv in start
         for i in eachindex(source)
