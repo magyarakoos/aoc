c42d0d91b7da944315fcc973bde304885d320cec
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-19 08:36:27 +0200

    arch descktop sync: 2024-04-19 08:36:27

--- 2023/05/part2.jl
+++ 2023/05/part2.jl
@@ -2,7 +2,6 @@ using Intervals
 f = open("input", "r")
 start = Vector{Interval{Closed, Closed}}()
 m = map(x -> parse(Int, x), split(match(r"\S+(.*)", readline(f))[1]))
-
 for i in 2:2:length(m)
-    println(m[i - 1])
+    push!(start, Interval{Closed,(m[i - 1], m[i]))
 end
