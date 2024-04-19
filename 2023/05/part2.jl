80e3f7b9af487841f9838ec17d9a9f91c2eb735b
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-19 09:08:58 +0200

    arch descktop sync: 2024-04-19 09:08:58

--- 2023/05/part2.jl
+++ 2023/05/part2.jl
@@ -24,6 +24,7 @@ while !eof(f)
     for intv in start
         for i in eachindex(source)
             sect = intv ∩ source[i]
+            # println((intv, source[i]))
             if !isempty(sect)
                 push!(curr, Interval{Closed, Open}(
                     first(intv) + diff[i], 
