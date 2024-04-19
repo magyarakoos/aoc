1b1f7c4599724fc26eb7fc84d6f92e4f51899085
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-19 10:33:21 +0200

    arch descktop sync: 2024-04-19 10:33:21

--- 2023/05/part2.jl
+++ 2023/05/part2.jl
@@ -20,6 +20,9 @@ while !eof(f)
         push!(diff, l[1] - l[2])
     end
 
+    if !eof(f)
+    
+    end
     push!(source, Interval{Closed, Open}(0, minimum(map(x -> first(x), source))))
     push!(source, Interval{Closed, Open}(maximum(map(x -> last(x), source)), typemax(Int)))
     for _ in 1:2 push!(diff, 0) end
