13cd0a198c9b002ef5e1baa830fd16021d08b816
magyarakoos <magyar.kende.22f@gmail.com>
2024-04-25 16:21:02 +0200

    arch descktop sync: 2024-04-25 16:21:02

--- 2023/20/main.cpp
+++ 2023/20/main.cpp
@@ -9,10 +9,11 @@ int main() {
     ifstream f("input");
     string s;
     while (getline(f, s)) {
-        replace(s.begin(), s.end(), '')
+        replace(s.begin(), s.end(), ',', ' ');
         istringstream iss(s);
         vector<string> t;
         while (iss >> s) t.push_back(s);
         m[t[0].substr(1)] = {t[0][0], 0};
+        for ()
     }
 }
